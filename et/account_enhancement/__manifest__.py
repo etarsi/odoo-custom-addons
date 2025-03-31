0db68629f8690b882508ec7e63859da445ed3f7a
etarsi <tarsitano.ezquiel@hotmail.com>
2025-03-31 03:04:40 -0300

    fix

--- et/account_enhancement/__manifest__.py
+++ et/account_enhancement/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['account_check_printing'],
+    'depends': ['account_check_printing', 'account-payment'],
 
     # always loaded
     'data': [
