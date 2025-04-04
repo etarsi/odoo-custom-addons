f70d7ef170227114c07d4ee87bbabbba79cc87ac
etarsi <tarsitano.ezquiel@hotmail.com>
2025-04-04 15:26:43 -0300

    .

--- et/custom_scripts/update_product_names.py
+++ et/custom_scripts/update_product_names.py
@@ -20,7 +20,7 @@ results = []
 
 context = {'lang': 'es_AR'}
 
-products_ids = models.execute_kw(db, uid, password, 'product.template', 'search', [[('detailed_type', '=', 'product'), ('default_code', '=', '55708')]], {'context': context})
+products_ids = models.execute_kw(db, uid, password, 'product.template', 'search', [[('detailed_type', '=', 'product')]], {'context': context})
 products_data = models.execute_kw(db, uid, password, 'product.template', 'read', [products_ids], {'fields': ['name'], 'context': context})
 
 # output_file = "/opt/odoo15/odoo-custom-addons/et/custom_scripts/products_ids.txt"
@@ -33,8 +33,8 @@ for p in products_data:
     cleaned_name = re.sub(r'\s+', ' ', cleaned_name)
 
     print(f"raw_name: {p['name']}")
-    # if raw_name != cleaned_name:
-    #     models.execute_kw(db, uid, password, 'product.template', 'write', [[product_id], {'name': cleaned_name}], {'context': context})
-    #     print(f"Producto {product_id} actualizado: {raw_name} -> {cleaned_name}")
-    # else:
-    #     print("Los nombres no coinciden")
+    if raw_name != cleaned_name:
+        models.execute_kw(db, uid, password, 'product.template', 'write', [[product_id], {'name': cleaned_name}], {'context': context})
+        print(f"Producto {product_id} actualizado: {raw_name} -> {cleaned_name}")
+    else:
+        print("Los nombres no coinciden")
