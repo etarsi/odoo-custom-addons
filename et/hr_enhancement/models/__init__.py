33515da373c9a4161335b72e8c575041bf88f65c
etarsi <tarsitano.ezquiel@hotmail.com>
2025-04-07 15:19:06 -0300

    fix

--- et/hr_enhancement/models/__init__.py
+++ et/hr_enhancement/models/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
 from . import models
-# from . import employee_license
-# from . import employee_children
+from . import employee_license
+from . import employee_children
--- et/hr_enhancement/models/models.py
+++ et/hr_enhancement/models/models.py
@@ -12,38 +12,38 @@ class HrEmployee(models.Model):
     cuil = fields.Integer('CUIL')
     dni_fotos = fields.Char('Foto DNI')
     hijos = fields.Boolean('Hijos', default=False)
-    # hijos_datos = fields.One2many('employee.children', 'parent_id', string='Hijos')    
+    hijos_datos = fields.One2many('employee.children', 'parent_id', string='Hijos')    
     alta_afip = fields.Date('Fecha de Alta AFIP')
-    # licencias = fields.One2many('employee.license', 'employee_id', string="Licencias")
+    licencias = fields.One2many('employee.license', 'employee_id', string="Licencias")
     licencia_count = fields.Integer(string="Cantidad de Licencias", compute='_compute_licencia_count')
 
-    # @api.depends('licencias')
-    # def _compute_licencia_count(self):
-    #     for rec in self:
-    #         rec.licencia_count = len(rec.licencias)
+    @api.depends('licencias')
+    def _compute_licencia_count(self):
+        for rec in self:
+            rec.licencia_count = len(rec.licencias)
     
-    # @api.model
-    # def action_open_my_profile(self):
-    #     employee = self.env['hr.employee'].search([('user_id', '=', self.env.uid)], limit=1)
-    #     if employee:
-    #         return {
-    #             'type': 'ir.actions.act_window',
-    #             'name': 'Mi Perfil',
-    #             'res_model': 'hr.employee',
-    #             'view_mode': 'form',
-    #             'res_id': employee.id,
-    #             'target': 'current',
-    #         }
-    #     else:
-    #         return {'type': 'ir.actions.act_window_close'}
+    @api.model
+    def action_open_my_profile(self):
+        employee = self.env['hr.employee'].search([('user_id', '=', self.env.uid)], limit=1)
+        if employee:
+            return {
+                'type': 'ir.actions.act_window',
+                'name': 'Mi Perfil',
+                'res_model': 'hr.employee',
+                'view_mode': 'form',
+                'res_id': employee.id,
+                'target': 'current',
+            }
+        else:
+            return {'type': 'ir.actions.act_window_close'}
 
-    # def action_view_licencias(self):
-    #     self.ensure_one()
-    #     return {
-    #         'name': 'Licencias',
-    #         'type': 'ir.actions.act_window',
-    #         'res_model': 'employee.license',
-    #         'view_mode': 'tree,form',
-    #         'domain': [('employee_id', '=', self.id)],
-    #         'context': {'default_employee_id': self.id},
-    #     }
+    def action_view_licencias(self):
+        self.ensure_one()
+        return {
+            'name': 'Licencias',
+            'type': 'ir.actions.act_window',
+            'res_model': 'employee.license',
+            'view_mode': 'tree,form',
+            'domain': [('employee_id', '=', self.id)],
+            'context': {'default_employee_id': self.id},
+        }
