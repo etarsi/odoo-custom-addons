637c5e5741e900f6fcc43b28cabfff11569590b4
etarsi <tarsitano.ezquiel@hotmail.com>
2025-04-08 12:09:43 -0300

    .

--- et/stock_enhancement2/models/models.py
+++ et/stock_enhancement2/models/models.py
@@ -23,3 +23,24 @@ class StockPickingInherit(models.Model):
         related='sale_id.condicion_m2m',
         store=True
     )
+
+    def split_auto(self):
+        for picking in self:
+            selected_moves = self.env['stock.move']
+            line_count = 0
+            bulto_count = 0
+
+            for move in picking.move_ids_without_package:
+                if move.product_available_percent == 100:
+                    if line_count > 24 or bulto_count >= 15:
+                        break
+                    
+                    bulto_qty = bulto_count + move.product_packaging_qty
+                    if bulto_qty <= 15:            
+                        line_count += 1
+                        bulto_count += move.product_packaging_qty
+                        selected_moves |= move
+
+            if selected_moves:
+                return picking._split_off_moves(selected_moves)
+        return False
