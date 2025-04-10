ce8fa581c9986a0ef35935f90084058f143e0688
etarsi <tarsitano.ezquiel@hotmail.com>
2025-04-10 16:58:57 -0300

    .

--- et/sale_enhancement/models/models.py
+++ et/sale_enhancement/models/models.py
@@ -220,4 +220,4 @@ class SaleOrderLineInherit(models.Model):
         _inherit = 'res.partner'
 
 
-        property_delivery_carrier_id = fields.Many2one('delivery.carrier', company_dependent=False, string="Delivery Method", help="Default delivery method used in sales orders.")
+        property_delivery_carrier_id = fields.Many2one('delivery.carrier', company_dependent=True, string="Delivery Method", help="Default delivery method used in sales orders.")
