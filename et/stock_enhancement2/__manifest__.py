81570a30b6f0d5da24cbeab423edb2d8c3b0bb74
etarsi <tarsitano.ezquiel@hotmail.com>
2025-04-11 11:06:17 -0300

    .

--- et/stock_enhancement2/__manifest__.py
+++ et/stock_enhancement2/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['stock', 'digipwms-mainv2'],
+    'depends': ['stock', 'digipwms-mainv2', 'sebigus-varios'],
 
     # always loaded
     'data': [
