ec04a2cb578e907571a487538c45eb2d45a375d4
etarsi <tarsitano.ezquiel@hotmail.com>
2025-04-14 17:25:07 -0300

    .

--- et/stock_enhancement2/models/models.py
+++ et/stock_enhancement2/models/models.py
@@ -57,7 +57,7 @@ class StockPickingInherit(models.Model):
         return {
             'type': 'ir.actions.act_url',
             'url': f'/remito/auto/{self.id}',
-            'target': 'self',
+            'target': 'new',
         }
     
     def _prepare_remito_data(self, picking, proportion, company_id):
