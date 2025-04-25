be147fce5fc0c009798acbdee3c31b11d6505196
etarsi <tarsitano.ezquiel@hotmail.com>
2025-04-25 19:42:26 -0300

    .

--- et/stock_enhancement2/__manifest__.py
+++ et/stock_enhancement2/__manifest__.py
@@ -26,7 +26,7 @@
     },
 
     # any module necessary for this one to work correctly
-    'depends': ['stock', 'stock-packaging'],
+    'depends': ['stock', 'stock-packaging', 'sale'],
 
     # always loaded
     'data': [
