64cbb85a4bdef98fa3f051921523914653ce034a
etarsi <tarsitano.ezquiel@hotmail.com>
2025-04-28 14:27:41 -0300

    .

--- sebigus-varios/stock-packaging/models/sale_order.py
+++ sebigus-varios/stock-packaging/models/sale_order.py
@@ -5,21 +5,28 @@ from odoo.exceptions import ValidationError
 import logging
 _logger = logging.getLogger(__name__)
 
-# class SaleOrder(models.Model):
-#     _inherit = 'sale.order'
-#     packaging_qty = fields.Float(string='Cantidad de Bultos' ,compute='get_bultos')
-#     qty_delivered = fields.Float(string='Entregados',compute='get_bultos',store=True)
-#     qty_invoiced = fields.Float(string='Facturados',compute='get_bultos')
-#     sale_order_lines = fields.Float(string='Lineas de Pedido',compute='get_lines')
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+    # packaging_qty = fields.Float(string='Cantidad de Bultos' ,compute='get_bultos')
+    # qty_delivered = fields.Float(string='Entregados',compute='get_bultos',store=True)
+    # qty_invoiced = fields.Float(string='Facturados',compute='get_bultos')
+    # sale_order_lines = fields.Float(string='Lineas de Pedido',compute='get_lines')
+    
+    packaging_qty = fields.Float(string='Cantidad de Bultos' ,compute='get_bultos')
+    qty_delivered = fields.Float(string='Entregados',compute='get_bultos',store=True)
+    qty_invoiced = fields.Float(string='Facturados',compute='get_bultos')
+    sale_order_lines = fields.Float(string='Lineas de Pedido',compute='get_lines')
 
-#     def get_lines(self):
+    def get_lines(self):
+        return
 #         order_lines = 0
 #         for order in self:      
 #             for line in order.order_line:      
 #                 order_lines = len(order.order_line)
 #             order.update({'sale_order_lines': order_lines })
             
-#     def get_bultos(self):
+    def get_bultos(self):
+        return
 #         self.packaging_qty = 0
 #         for order in self:      
 #             bultos = 0.0       
