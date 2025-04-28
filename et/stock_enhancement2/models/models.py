39608ddf4f804cf4c0c4eaf74387104d2c459d05
etarsi <tarsitano.ezquiel@hotmail.com>
2025-04-28 05:35:01 -0300

    .

--- et/stock_enhancement2/models/models.py
+++ et/stock_enhancement2/models/models.py
@@ -460,168 +460,168 @@ class StockMoveInherit(models.Model):
 class SaleOrderInherit(models.Model):
     _inherit = 'sale.order'
 
-    def _create_invoices(self, grouped=False, final=False, date=None):
-        """
-        Create the invoice associated to the SO.
-        :param grouped: if True, invoices are grouped by SO id. If False, invoices are grouped by
-                        (partner_invoice_id, currency)
-        :param final: if True, refunds will be generated if necessary
-        :returns: list of created invoices
-        """
-        if not self.env['account.move'].check_access_rights('create', False):
-            try:
-                self.check_access_rights('write')
-                self.check_access_rule('write')
-            except AccessError:
-                return self.env['account.move']
+    # def _create_invoices(self, grouped=False, final=False, date=None):
+    #     """
+    #     Create the invoice associated to the SO.
+    #     :param grouped: if True, invoices are grouped by SO id. If False, invoices are grouped by
+    #                     (partner_invoice_id, currency)
+    #     :param final: if True, refunds will be generated if necessary
+    #     :returns: list of created invoices
+    #     """
+    #     if not self.env['account.move'].check_access_rights('create', False):
+    #         try:
+    #             self.check_access_rights('write')
+    #             self.check_access_rule('write')
+    #         except AccessError:
+    #             return self.env['account.move']
 
-        # 1) Create invoices.
-        invoice_vals_list = []
-        invoice_item_sequence = 0 # Incremental sequencing to keep the lines order on the invoice.
-        for order in self:
-            order = order.with_company(order.company_id)
-            current_section_vals = None
-            down_payments = order.env['sale.order.line']
+    #     # 1) Create invoices.
+    #     invoice_vals_list = []
+    #     invoice_item_sequence = 0 # Incremental sequencing to keep the lines order on the invoice.
+    #     for order in self:
+    #         order = order.with_company(order.company_id)
+    #         current_section_vals = None
+    #         down_payments = order.env['sale.order.line']
 
-            invoice_vals = order._prepare_invoice()
-            invoiceable_lines = order._get_invoiceable_lines(final)
+    #         invoice_vals = order._prepare_invoice()
+    #         invoiceable_lines = order._get_invoiceable_lines(final)
 
-            if not any(not line.display_type for line in invoiceable_lines):
-                continue
+    #         if not any(not line.display_type for line in invoiceable_lines):
+    #             continue
 
-            tipo = (order.condicion_m2m.name or '').upper().strip()
-            proportion_blanco, proportion_negro = {
-                'TIPO 1': (1.0, 0.0),
-                'TIPO 2': (0.5, 0.5),
-                'TIPO 3': (0.0, 1.0),
-                'TIPO 4': (0.25, 0.75),
-            }.get(tipo, (1.0, 0.0))
+    #         tipo = (order.condicion_m2m.name or '').upper().strip()
+    #         proportion_blanco, proportion_negro = {
+    #             'TIPO 1': (1.0, 0.0),
+    #             'TIPO 2': (0.5, 0.5),
+    #             'TIPO 3': (0.0, 1.0),
+    #             'TIPO 4': (0.25, 0.75),
+    #         }.get(tipo, (1.0, 0.0))
 
-            company_negra = self.env['res.company'].browse(1)
+    #         company_negra = self.env['res.company'].browse(1)
 
-            invoice_line_vals_blanco = []
-            invoice_line_vals_negro = []
+    #         invoice_line_vals_blanco = []
+    #         invoice_line_vals_negro = []
 
-            for line in invoiceable_lines:
-                base_vals = line._prepare_invoice_line(sequence=invoice_item_sequence)
+    #         for line in invoiceable_lines:
+    #             base_vals = line._prepare_invoice_line(sequence=invoice_item_sequence)
 
-                if proportion_blanco > 0:
-                    blanco_vals = base_vals.copy()
-                    blanco_vals['quantity'] = base_vals['quantity'] * proportion_blanco
+    #             if proportion_blanco > 0:
+    #                 blanco_vals = base_vals.copy()
+    #                 blanco_vals['quantity'] = base_vals['quantity'] * proportion_blanco
 
-                    impuestos_blancos = line.product_id.taxes_id.filtered(lambda t: t.company_id.id == order.company_id.id)
-                    blanco_vals['tax_ids'] = [(6, 0, impuestos_blancos.ids)]
+    #                 impuestos_blancos = line.product_id.taxes_id.filtered(lambda t: t.company_id.id == order.company_id.id)
+    #                 blanco_vals['tax_ids'] = [(6, 0, impuestos_blancos.ids)]
 
-                    invoice_line_vals_blanco.append((0, 0, blanco_vals))
+    #                 invoice_line_vals_blanco.append((0, 0, blanco_vals))
 
-                if proportion_negro > 0:
-                    negro_vals = base_vals.copy()
-                    negro_vals['quantity'] = base_vals['quantity'] * proportion_negro
+    #             if proportion_negro > 0:
+    #                 negro_vals = base_vals.copy()
+    #                 negro_vals['quantity'] = base_vals['quantity'] * proportion_negro
 
-                    impuestos_negros = line.product_id.taxes_id.filtered(lambda t: t.company_id.id == company_negra.id)
-                    negro_vals['tax_ids'] = [(6, 0, impuestos_negros.ids)]
+    #                 impuestos_negros = line.product_id.taxes_id.filtered(lambda t: t.company_id.id == company_negra.id)
+    #                 negro_vals['tax_ids'] = [(6, 0, impuestos_negros.ids)]
 
-                    invoice_line_vals_negro.append((0, 0, negro_vals))
+    #                 invoice_line_vals_negro.append((0, 0, negro_vals))
 
-                invoice_item_sequence += 1
+    #             invoice_item_sequence += 1
 
-            # Crear invoice blanco si corresponde
-            if invoice_line_vals_blanco:
-                invoice_vals_blanco = order._prepare_invoice()
-                invoice_vals_blanco['invoice_line_ids'] += invoice_line_vals_blanco
-                invoice_vals_blanco['company_id'] = order.company_id.id
-                invoice_vals_list.append(invoice_vals_blanco)
+    #         # Crear invoice blanco si corresponde
+    #         if invoice_line_vals_blanco:
+    #             invoice_vals_blanco = order._prepare_invoice()
+    #             invoice_vals_blanco['invoice_line_ids'] += invoice_line_vals_blanco
+    #             invoice_vals_blanco['company_id'] = order.company_id.id
+    #             invoice_vals_list.append(invoice_vals_blanco)
 
-            # Crear invoice negro si corresponde
-            if invoice_line_vals_negro:
-                invoice_vals_negro = order._prepare_invoice()
+    #         # Crear invoice negro si corresponde
+    #         if invoice_line_vals_negro:
+    #             invoice_vals_negro = order._prepare_invoice()
 
-                journal = self.env['account.journal'].browse(1)
+    #             journal = self.env['account.journal'].browse(1)
 
-                if not journal:
-                    raise UserError("No se encontró un diario de ventas para la compañía negra.")
+    #             if not journal:
+    #                 raise UserError("No se encontró un diario de ventas para la compañía negra.")
                 
-                invoice_vals_negro['company_id'] = company_negra.id
-                invoice_vals_negro['journal_id'] = journal.id
-                invoice_vals_negro['partner_bank_id'] = False
-                invoice_vals_negro['invoice_line_ids'] += invoice_line_vals_negro
-                invoice_vals_list.append(invoice_vals_negro)
+    #             invoice_vals_negro['company_id'] = company_negra.id
+    #             invoice_vals_negro['journal_id'] = journal.id
+    #             invoice_vals_negro['partner_bank_id'] = False
+    #             invoice_vals_negro['invoice_line_ids'] += invoice_line_vals_negro
+    #             invoice_vals_list.append(invoice_vals_negro)
 
 
-        if not invoice_vals_list:
-            raise self._nothing_to_invoice_error()
+    #     if not invoice_vals_list:
+    #         raise self._nothing_to_invoice_error()
 
-        # 2) Manage 'grouped' parameter: group by (partner_id, currency_id).
-        if not grouped:
-            new_invoice_vals_list = []
-            invoice_grouping_keys = self._get_invoice_grouping_keys()
-            invoice_vals_list = sorted(
-                invoice_vals_list,
-                key=lambda x: [
-                    x.get(grouping_key) for grouping_key in invoice_grouping_keys
-                ]
-            )
-            for grouping_keys, invoices in groupby(invoice_vals_list, key=lambda x: [x.get(grouping_key) for grouping_key in invoice_grouping_keys]):
-                origins = set()
-                payment_refs = set()
-                refs = set()
-                ref_invoice_vals = None
-                for invoice_vals in invoices:
-                    if not ref_invoice_vals:
-                        ref_invoice_vals = invoice_vals
-                    else:
-                        ref_invoice_vals['invoice_line_ids'] += invoice_vals['invoice_line_ids']
-                    origins.add(invoice_vals['invoice_origin'])
-                    payment_refs.add(invoice_vals['payment_reference'])
-                    refs.add(invoice_vals['ref'])
-                ref_invoice_vals.update({
-                    'ref': ', '.join(refs)[:2000],
-                    'invoice_origin': ', '.join(origins),
-                    'payment_reference': len(payment_refs) == 1 and payment_refs.pop() or False,
-                })
-                new_invoice_vals_list.append(ref_invoice_vals)
-            invoice_vals_list = new_invoice_vals_list
+    #     # 2) Manage 'grouped' parameter: group by (partner_id, currency_id).
+    #     if not grouped:
+    #         new_invoice_vals_list = []
+    #         invoice_grouping_keys = self._get_invoice_grouping_keys()
+    #         invoice_vals_list = sorted(
+    #             invoice_vals_list,
+    #             key=lambda x: [
+    #                 x.get(grouping_key) for grouping_key in invoice_grouping_keys
+    #             ]
+    #         )
+    #         for grouping_keys, invoices in groupby(invoice_vals_list, key=lambda x: [x.get(grouping_key) for grouping_key in invoice_grouping_keys]):
+    #             origins = set()
+    #             payment_refs = set()
+    #             refs = set()
+    #             ref_invoice_vals = None
+    #             for invoice_vals in invoices:
+    #                 if not ref_invoice_vals:
+    #                     ref_invoice_vals = invoice_vals
+    #                 else:
+    #                     ref_invoice_vals['invoice_line_ids'] += invoice_vals['invoice_line_ids']
+    #                 origins.add(invoice_vals['invoice_origin'])
+    #                 payment_refs.add(invoice_vals['payment_reference'])
+    #                 refs.add(invoice_vals['ref'])
+    #             ref_invoice_vals.update({
+    #                 'ref': ', '.join(refs)[:2000],
+    #                 'invoice_origin': ', '.join(origins),
+    #                 'payment_reference': len(payment_refs) == 1 and payment_refs.pop() or False,
+    #             })
+    #             new_invoice_vals_list.append(ref_invoice_vals)
+    #         invoice_vals_list = new_invoice_vals_list
 
-        # 3) Create invoices.
+    #     # 3) Create invoices.
 
-        # As part of the invoice creation, we make sure the sequence of multiple SO do not interfere
-        # in a single invoice. Example:
-        # SO 1:
-        # - Section A (sequence: 10)
-        # - Product A (sequence: 11)
-        # SO 2:
-        # - Section B (sequence: 10)
-        # - Product B (sequence: 11)
-        #
-        # If SO 1 & 2 are grouped in the same invoice, the result will be:
-        # - Section A (sequence: 10)
-        # - Section B (sequence: 10)
-        # - Product A (sequence: 11)
-        # - Product B (sequence: 11)
-        #
-        # Resequencing should be safe, however we resequence only if there are less invoices than
-        # orders, meaning a grouping might have been done. This could also mean that only a part
-        # of the selected SO are invoiceable, but resequencing in this case shouldn't be an issue.
-        if len(invoice_vals_list) < len(self):
-            SaleOrderLine = self.env['sale.order.line']
-            for invoice in invoice_vals_list:
-                sequence = 1
-                for line in invoice['invoice_line_ids']:
-                    line[2]['sequence'] = SaleOrderLine._get_invoice_line_sequence(new=sequence, old=line[2]['sequence'])
-                    sequence += 1
+    #     # As part of the invoice creation, we make sure the sequence of multiple SO do not interfere
+    #     # in a single invoice. Example:
+    #     # SO 1:
+    #     # - Section A (sequence: 10)
+    #     # - Product A (sequence: 11)
+    #     # SO 2:
+    #     # - Section B (sequence: 10)
+    #     # - Product B (sequence: 11)
+    #     #
+    #     # If SO 1 & 2 are grouped in the same invoice, the result will be:
+    #     # - Section A (sequence: 10)
+    #     # - Section B (sequence: 10)
+    #     # - Product A (sequence: 11)
+    #     # - Product B (sequence: 11)
+    #     #
+    #     # Resequencing should be safe, however we resequence only if there are less invoices than
+    #     # orders, meaning a grouping might have been done. This could also mean that only a part
+    #     # of the selected SO are invoiceable, but resequencing in this case shouldn't be an issue.
+    #     if len(invoice_vals_list) < len(self):
+    #         SaleOrderLine = self.env['sale.order.line']
+    #         for invoice in invoice_vals_list:
+    #             sequence = 1
+    #             for line in invoice['invoice_line_ids']:
+    #                 line[2]['sequence'] = SaleOrderLine._get_invoice_line_sequence(new=sequence, old=line[2]['sequence'])
+    #                 sequence += 1
 
-        # Manage the creation of invoices in sudo because a salesperson must be able to generate an invoice from a
-        # sale order without "billing" access rights. However, he should not be able to create an invoice from scratch.
-        moves = self.env['account.move'].sudo().with_context(default_move_type='out_invoice').create(invoice_vals_list)
+    #     # Manage the creation of invoices in sudo because a salesperson must be able to generate an invoice from a
+    #     # sale order without "billing" access rights. However, he should not be able to create an invoice from scratch.
+    #     moves = self.env['account.move'].sudo().with_context(default_move_type='out_invoice').create(invoice_vals_list)
 
-        # 4) Some moves might actually be refunds: convert them if the total amount is negative
-        # We do this after the moves have been created since we need taxes, etc. to know if the total
-        # is actually negative or not
-        if final:
-            moves.sudo().filtered(lambda m: m.amount_total < 0).action_switch_invoice_into_refund_credit_note()
-        for move in moves:
-            move.message_post_with_view('mail.message_origin_link',
-                values={'self': move, 'origin': move.line_ids.mapped('sale_line_ids.order_id')},
-                subtype_id=self.env.ref('mail.mt_note').id
-            )
-        return moves
+    #     # 4) Some moves might actually be refunds: convert them if the total amount is negative
+    #     # We do this after the moves have been created since we need taxes, etc. to know if the total
+    #     # is actually negative or not
+    #     if final:
+    #         moves.sudo().filtered(lambda m: m.amount_total < 0).action_switch_invoice_into_refund_credit_note()
+    #     for move in moves:
+    #         move.message_post_with_view('mail.message_origin_link',
+    #             values={'self': move, 'origin': move.line_ids.mapped('sale_line_ids.order_id')},
+    #             subtype_id=self.env.ref('mail.mt_note').id
+    #         )
+    #     return moves
