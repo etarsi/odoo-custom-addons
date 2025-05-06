253e55ac3bef5c472c3e5ce644c1f79af7311c0c
etarsi <tarsitano.ezquiel@hotmail.com>
2025-05-06 15:56:03 -0300

    .

--- et/sale_enhancement/models/models.py
+++ et/sale_enhancement/models/models.py
@@ -18,6 +18,8 @@ class SaleOrderInherit(models.Model):
     special_price = fields.Boolean('Precios especiales')
     order_subtotal = fields.Float('Subtotal', compute='_compute_subtotal', readonly=True)
     global_discount = fields.Float('Descuento', default=0)
+    old_sale = fields.Boolean(default=False)
+    old_sale_txt = fields.Text(default='⚠️ PICKING VIEJO - FACTURAR E IMPRIMIR REMITO DE LA VIEJA FORMA')
 
     def action_confirm(self):
         res = super().action_confirm()
