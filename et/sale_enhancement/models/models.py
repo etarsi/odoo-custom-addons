5b4ec9aa39442928d665fb5ec70a00f862cd464d
etarsi <tarsitano.ezquiel@hotmail.com>
2025-05-06 16:56:37 -0300

    .

--- et/sale_enhancement/models/models.py
+++ et/sale_enhancement/models/models.py
@@ -215,6 +215,8 @@ class SaleOrderSplitWizardInherit(models.TransientModel):
         default=lambda self: self._default_percentage()
     )
 
+    old_sale = fields.Boolean(default=False)
+
 
     def _default_percentage(self):
         sale_order_id = self._context.get('active_id')
