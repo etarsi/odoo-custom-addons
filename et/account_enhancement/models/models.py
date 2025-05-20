09e73c9eedf3f3b8f791f9dd6ad42a758a34c61c
etarsi <tarsitano.ezquiel@hotmail.com>
2025-05-20 13:56:32 -0300

    .

--- et/account_enhancement/models/models.py
+++ et/account_enhancement/models/models.py
@@ -46,6 +46,11 @@ class AccountPaymentInherit(models.Model):
     no_diferido = fields.Boolean('No diferido', default=False)
     no_a_la_orden = fields.Boolean('No a la Orden', default=False)
 
+    @api.onchange('date')
+    def _onchange_date(self):
+        for record in self:
+            record.date = record.payment_group_id.payment_date
+
     @api.onchange('no_diferido')
     def _onchange_no_diferido(self):
         if self.no_diferido:
@@ -71,6 +76,11 @@ class AccountPaymentInherit(models.Model):
 class AccountPaymentGroupInherit(models.Model):
     _inherit = 'account.payment.group'
 
+    @api.onchange('payment_date')
+    def _onchange_payment_date(self):
+        for record in self:
+            for payment_line in record.payment_ids:
+                payment_line.date = record.payment_date
 
     def post(self):
         """ Post payment group. If payment is created automatically when creating a payment (for eg. from website
