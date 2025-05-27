4af40359bc4253c4273552744891f875471c6d07
etarsi <tarsitano.ezquiel@hotmail.com>
2025-05-27 09:59:43 -0300

    .

--- et/account_enhancement/models/models.py
+++ et/account_enhancement/models/models.py
@@ -221,8 +221,29 @@ class SaleOrderInherit(models.Model):
         }
         return invoice_vals
     
-    class WmsCode(models.Model):
-        _name = "wms.code"
-        _description = "Código WMS"
+class WmsCode(models.Model):
+    _name = "wms.code"
+    _description = "Código WMS"
 
-        name = fields.Char("Código", required=True)
+    name = fields.Char("Código", required=True)
+
+
+class AccountPaymentInherit(models.Model):
+    _inherit = 'account.payment.mass.transfer'
+
+
+    @api.model
+    def default_get(self, fields_list):
+        res = super().default_get(fields_list)
+        if self._context.get('active_model') != 'account.payment':
+            raise UserError(_("The register payment wizard should only be called on account.payment records."))
+        payments = self.env['account.payment'].browse(self._context.get('active_ids', []))
+        checks = payments.filtered(lambda x: x.payment_method_line_id.code == 'new_third_party_checks')
+        if not all(check.state == 'posted' for check in checks):
+            raise UserError(_("All the selected checks must be posted"))
+        self.filtered(lambda x: x.payment_method_line_id.code in ['in_third_party_checks', 'out_third_party_checks'])
+        if not checks[0].l10n_latam_check_current_journal_id.inbound_payment_method_line_ids.filtered(
+                lambda x: x.code == 'in_third_party_checks'):
+            raise UserError(_("Checks must be on a third party checks journal to be transfered by this wizard"))
+        res['journal_id'] = checks[0].l10n_latam_check_current_journal_id.id
+        return res
