3175f2311369d667abcd06af5f100f48768fdf71
etarsi <tarsitano.ezquiel@hotmail.com>
2025-05-27 10:00:44 -0300

    .

--- et/account_enhancement/__manifest__.py
+++ et/account_enhancement/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['account_check_printing', 'account_payment_group', 'l10n_latam_check'],
+    'depends': ['account_check_printing', 'account_payment_group'],
 
     # always loaded
     'data': [
