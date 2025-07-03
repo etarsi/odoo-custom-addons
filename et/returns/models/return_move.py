501ef14e166f63958ee20920e0a3efdec4e52523
etarsi <tarsitano.ezquiel@hotmail.com>
2025-07-03 10:56:20 -0300

    .

--- et/returns/models/return_move.py
+++ et/returns/models/return_move.py
@@ -5,12 +5,20 @@ class ReturnMove(models.Model):
 
     name = fields.Char(string="Nombre", required=True, default="Remito de prueba")
     partner_id = fields.Many2one('res.partner', string="Cliente")
-    sale_id = fields.Many2one()
-    invoice_id = fields.Many2one()
-    cause = fields.Selection()
-    date = fields.Date()
-    state = fields.Selection()
-    move_lines = fields.One2many()
+    sale_id = fields.Many2one('sale.order', string="Pedido de Venta")
+    invoice_id = fields.Many2one('account.move', string="Factura")
+    cause = fields.Selection(string="Motivo", default=False, selection=[
+        ('error', 'Producto Erróneo'),
+        ('broken','Producto Roto'),
+        ('no', 'No lo quiere'),
+        ('expensive', 'Muy caro'),
+        ('bad', 'No lo pudo vender')])
+    info = fields.Text(string="Información adicional")
+    date = fields.Date(string="Fecha de Recepción")
+    state = fields.Selection(string="Estado", default='draft', selection=[('draft','Borrador'), ('confirmed', 'Confirmado'), ('done', 'Hecho')])
+    move_lines = fields.One2many('return.move.line', string="Líneas de Devolución")
+    price_total = fields.Float(string="Total", compute="_compute_price_total")
+    company_id = fields.Many2one('res.company', string="Compañía")
 
     
 
@@ -19,9 +27,9 @@ class ReturnMoveLine(models.Model):
     _name = 'return.move.line'
 
     name = fields.Char(string="Nombre", required=True, default="Remito de prueba")
-    product_id = fields.Many2one()
-    quantity = fields.Integer()
-    uxb = fields.Integer()
-    bultos = fields.Float()
-    
-    
+    return_move = fields.Many2one('return.move', string="Devolución")
+    product_id = fields.Many2one('product.product', string="Producto")
+    quantity = fields.Integer(string="Cantidad")
+    uxb = fields.Integer(string="UxB")
+    bultos = fields.Float(string="Bultos")
+    price_unit = fields.Float(string="Precio Unitario")
