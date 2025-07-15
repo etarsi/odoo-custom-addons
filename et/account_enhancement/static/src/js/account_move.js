278da4efecfa5b81512e56253f5dece88b8e6924
ab-iceberg <rbmaldonado.1scz@gmail.com>
2025-07-15 16:25:34 -0300

    fix: se coloco unos log para debug

--- et/account_enhancement/static/src/js/account_move.js
+++ et/account_enhancement/static/src/js/account_move.js
@@ -1,22 +1,24 @@
-odoo.define('account_enhancement.TaxGroupComponent', function (require) {
+odoo.define('account_enhancement.TaxTotalsComponent', function (require) {
     "use strict";
 
-    const TaxGroupComponent = require('account.TaxGroupComponent');
+    const TaxTotalsComponent = require('account.TaxTotalsComponent');
     const session = require('web.session');
     const fieldUtils = require('web.field_utils');
     const { registry } = require('web.field_registry_owl');
 
-    class TaxGroupComponentEnhancement extends TaxGroupComponent {
-        _onChangeTaxValue() {
-            // 1. Primero, tu lógica personalizada antes del super
-            this.inputTax.el.value = this.inputTax.el.value.replace(',', '.');
-            // 2. Llamas al método original
-            super._onChangeTaxValue();
+    class TaxTotalsComponentEnhancement extends TaxTotalsComponent {
+        _onChangeTaxValueByTaxGroup(ev) {
+            console.log('TaxTotalsComponentEnhancement: _onChangeTaxValueByTaxGroup called');
+            console.log('Event:', this.totals.value);
+            this.trigger('field-changed', {
+                dataPointID: this.record.id,
+                changes: { tax_totals_json: JSON.stringify(this.totals.value) }
+            })
         }
     }
 
     // Registrar el componente extendido en el registry
-    registry.category('fields').add('TaxGroupComponent', TaxGroupComponentEnhancement);
+    registry.category('fields').add('TaxTotalsComponent', TaxTotalsComponentEnhancement);
 
     // Si lo usas en un template personalizado, puedes darle otro nombre en el registry
     // registry.category('fields').add('tax_group_component_enhancement', TaxGroupComponentEnhancement);
