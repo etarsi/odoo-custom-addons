a9bf9eae0cfd4ec2341b88943cd031d69d34b421
ab-iceberg <rbmaldonado.1scz@gmail.com>
2025-07-15 17:12:16 -0300

    fix: se agrego el manifest de ventas dependencias

--- et/sale_enhancement/__manifest__.py
+++ et/sale_enhancement/__manifest__.py
@@ -20,7 +20,8 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['sale', 'delivery', 'sebigus-split-orders'],
+    #'depends': ['sale', 'delivery', 'sebigus-split-orders'],
+    'depends': ['sale', 'delivery'],
 
     # always loaded
     'data': [
