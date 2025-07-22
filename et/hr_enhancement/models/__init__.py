c0aafa39cda3bfb081743bbe797fe0ceef06f5b0
ab-iceberg <rbmaldonado.1scz@gmail.com>
2025-07-22 15:19:05 -0300

    fix:correccion de moduelo de recursos humanos

--- et/hr_enhancement/models/__init__.py
+++ et/hr_enhancement/models/__init__.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-from . import hr_employee
 from . import hr_location
 from . import hr_employee_salary
 from . import hr_license
 from . import hr_employee_children
 from . import hr_license_type
 from . import hr_season_labor_cost
-
+from . import hr_employee
