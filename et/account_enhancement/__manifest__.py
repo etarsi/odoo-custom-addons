c263654e5af7da001ae44745e9e7f4ad1f5e6cbd
ab-iceberg <rbmaldonado.1scz@gmail.com>
2025-07-29 01:10:10 -0300

    fix: correcion en el reporte de factura

--- et/account_enhancement/__manifest__.py
+++ et/account_enhancement/__manifest__.py
@@ -30,7 +30,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         #Reportes
         'wizard/generar_factura_wizard_views.xml',
         #permissions
