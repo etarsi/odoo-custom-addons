2aa346ac7f23b3da4253cd4e2162ea28220699ea
etarsi <tarsitano.ezquiel@hotmail.com>
2025-08-07 16:47:04 -0300

    .

---
+++ et/custom_scripts/get_digip_providers.py
@@ -0,0 +1,18 @@
+import requests
+
+headers = {}
+params = {}
+
+url = "http://api.patagoniawms.com/v1/Proveedor"
+headers = {
+    "X-API-Key": "97ca7acf-9013-45cf-b287-16cfb2edc826",
+    "Content-Type": "application/json"
+}
+
+response = requests.get(url, headers=headers)
+
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+else:
+    print(f"Error {response.status_code}: {response.text}")
