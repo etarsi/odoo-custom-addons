eab9aafd2e0d3fb4877d815368a87156ba0f6f30
ab-iceberg <rbmaldonado.1scz@gmail.com>
2025-08-07 15:58:47 -0300

    fix: correcion en el rango de hora

--- et/hr_enhancement/models/hr_work_schedule.py
+++ et/hr_enhancement/models/hr_work_schedule.py
@@ -5,6 +5,7 @@ from odoo.exceptions import ValidationError
 class HrWorkSchedule(models.Model):
     _name = 'hr.work.schedule'
     _description = 'Rango de Horario Laboral'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
 
     type = fields.Selection([
         ('empleado', 'Empleado'),
@@ -22,12 +23,3 @@ class HrWorkSchedule(models.Model):
     _sql_constraints = [
         ('unique_tipo_activo', 'unique(tipo, state)', 'Sólo puede haber un horario ACTIVO por tipo.'),
     ]
-
-    @api.constrains('hour_start', 'hour_end', 'hour_start_night', 'hour_end_night')
-    def _check_hours(self):
-        for rec in self:
-            for field_name in ['hour_start', 'hour_end', 'hour_start_night', 'hour_end_night']:
-                value = getattr(rec, field_name)
-                if value is not False:  # None or 0 is valid
-                    if value < 0 or value >= 24:
-                        raise ValidationError("La hora en '%s' debe estar entre 00:00 y 23:59." % dict(self._fields[field_name].string))
