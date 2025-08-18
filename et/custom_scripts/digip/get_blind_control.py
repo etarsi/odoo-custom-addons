9c7fc1abd6f39b9ab0bf395f3e698c0064115d0f
etarsi <tarsitano.ezquiel@hotmail.com>
2025-08-18 16:59:32 -0300

    feat: sale order misiones validation / fix: remito address

---
+++ et/custom_scripts/digip/get_blind_control.py
@@ -0,0 +1,21 @@
+import requests
+
+headers = {}
+params = {}
+
+url = "http://api.patagoniawms.com/v1/ControlCiego"
+headers = {
+    "X-API-Key": "97ca7acf-9013-45cf-b287-16cfb2edc826",
+    "Content-Type": "application/json"
+}
+params = {
+    'DocumentoNumero': 'R00033',
+}
+
+response = requests.get(url, headers=headers, params=params)
+
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+else:
+    print(f"Error {response.status_code}: {response.text}")
