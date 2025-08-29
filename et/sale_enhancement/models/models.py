2b9da94b07c12b7fd2446e67f7153c471cd4601e
etarsi <tarsitano.ezquiel@hotmail.com>
2025-08-29 17:17:03 -0300

    fix: typo in dic t

--- et/sale_enhancement/models/models.py
+++ et/sale_enhancement/models/models.py
@@ -176,7 +176,7 @@ class SaleOrderInherit(models.Model):
                 vals['sale_id'] = record.id
                 vals['product_id'] = line.product_id.id
                 vals['quantity'] = line.product_uom_qty
-                vals['uxb'] = line.product_packaging_id.q or ''
+                vals['uxb'] = line.product_packaging_id.qty or ''
                 vals['bultos'] = line.product_packaging_qty
 
                 vals_list.append(vals)
