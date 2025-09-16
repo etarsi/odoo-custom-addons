1b29bb20b34a3d2d990ee0f66a28a77b5777c74e
ab-iceberg <rbmaldonado.1scz@gmail.com>
2025-09-16 14:01:25 -0300

    feat: se agrego el modulo de tms service

--- et/tms_service/__manifest__.py
+++ et/tms_service/__manifest__.py
@@ -12,7 +12,8 @@
 
     'author': "Sebigus",
     'website': "http://www.sebigus.com.ar",
-
+    'application': True,
+    'installable': True,
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
