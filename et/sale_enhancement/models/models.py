1eaa64f10bfb253e6fba45969104d253876867e1
unknown <tarsitano.ezequiel@hotmail.com>
2025-09-17 09:11:26 -0300

    .

--- et/sale_enhancement/models/models.py
+++ et/sale_enhancement/models/models.py
@@ -588,7 +588,7 @@ class SaleOrderLineInherit(models.Model):
 
 
             if isinstance(line.id, int) and line.order_id.state == 'draft' and line.product_id:
-
+                raise UserError("2")
                 stock_moves_erp = line.env['stock.moves.erp'].search([('sale_line_id', '=', line.id), ('type', '=', 'reserve')], limit=1)
 
                 if stock_moves_erp:
