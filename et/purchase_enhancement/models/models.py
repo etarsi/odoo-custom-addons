879f2cd1cbc17192c424e51190e5265567b74e00
unknown <tarsitano.ezequiel@hotmail.com>
2025-09-25 07:21:15 -0300

    fix uom

--- et/purchase_enhancement/models/models.py
+++ et/purchase_enhancement/models/models.py
@@ -5,3 +5,12 @@ class PurchaseInherit(models.Model):
     _inherit = 'purchase.order'
     
     china_purchase = fields.Boolean('Compra china', default=False)
+
+class PurchaseLineInherit(models.Model):
+    _inherit = 'purchase.order.line'
+    
+    @api.onchange('product_id')
+    def _onchange_product_id(self):
+        for record in self:
+            if record.product_id:
+                record.product_uom = 1
