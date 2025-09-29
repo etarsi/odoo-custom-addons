640bb1b4864b20f49508032467e16c456a06af3a
ab-iceberg <rbmaldonado.1scz@gmail.com>
2025-09-28 23:57:15 -0300

    fix: cambios traidos por ezequiel

--- et/web_enhancement/__manifest__.py
+++ et/web_enhancement/__manifest__.py
@@ -12,24 +12,20 @@
 
     'author': "Sebigus",
     'website': "http://www.one.sebigus.com.ar",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Uncategorized',
     'version': '0.1',
-
-    # any module necessary for this one to work correctly
     'depends': ['base', 'web', 'website', 'product'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/web_product_templates.xml',
+        'views/web_menu.xml',
         'views/views.xml',
         'views/templates.xml',
     ],
-    # only loaded in demonstration mode
+    'qweb': [
+        'views/web_product_templates.xml',
+    ],
     'demo': [
         'demo/demo.xml',
     ],
