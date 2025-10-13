0d0499097887badd19fb04ab4b40250254e407c5
ab-iceberg <rbmaldonado.1scz@gmail.com>
2025-10-13 12:45:49 -0300

    feat: se agrego el reporte de excel para el reporte de asistencia del empleado

--- et/hr_enhancement/wizards/hr_report_attendance_wizard.py
+++ et/hr_enhancement/wizards/hr_report_attendance_wizard.py
@@ -7,21 +7,6 @@ import io
 import xlsxwriter
 from . import excel
 
-MESES_ES = {
-    '01': 'Enero',
-    '02': 'Febrero',
-    '03': 'Marzo',
-    '04': 'Abril',
-    '05': 'Mayo',
-    '06': 'Junio',
-    '07': 'Julio',
-    '08': 'Agosto',
-    '09': 'Septiembre',
-    '10': 'Octubre',
-    '11': 'Noviembre',
-    '12': 'Diciembre',
-}
-
 class HrReportAttendanceWizard(models.TransientModel):
     _name = 'hr.report.attendance.wizard'
     _description = 'Wizard para Exportar Asistencias a Excel'
@@ -55,35 +40,41 @@ class HrReportAttendanceWizard(models.TransientModel):
         workbook = xlsxwriter.Workbook(output, {'in_memory': True})
         worksheet = workbook.add_worksheet('Asistencias')
         # Agrupados por rangos consecutivos con mismo ancho
-        worksheet.set_column(0, 0, 30)   # nro correlativo
-        worksheet.set_column(1, 1, 20)   # Empleado
-        worksheet.set_column(2, 2, 20)   # Fecha Ingreso
-        worksheet.set_column(3, 3, 15)   # Fecha Salida
-        worksheet.set_column(4, 4, 15)   # Tipo de Empleado
+        worksheet.set_column(0, 0, 30)   # Empleado
+        worksheet.set_column(1, 1, 20)   # Fecha Ingreso
+        worksheet.set_column(2, 2, 20)   # Fecha Salida
+        worksheet.set_column(3, 3, 10)   # Horas Trabajadas
+        worksheet.set_column(4, 4, 10)   # Horas Extras
+        worksheet.set_column(5, 5, 10)   # Horas Feriado
+        worksheet.set_column(6, 6, 20)   # Tipo de Empleado
+        worksheet.set_column(7, 7, 20)   # Turno Asignado
         
         #formato de celdas
         formato_encabezado = excel.formato_encabezado(workbook)
         formato_celdas_izquierda = excel.formato_celda_izquierda(workbook)
         formato_celdas_derecha = excel.formato_celda_derecha(workbook)
         formato_celdas_decimal = excel.formato_celda_decimal(workbook)
-        fmt_emp     = workbook.add_format({'bold': True, 'bg_color': '#DDEBF7', 'border': 1, 'align': 'left'})
+        fmt_emp = workbook.add_format({'bold': True, 'bg_color': '#DDEBF7', 'border': 1, 'align': 'left'})
             
         # Escribir datos
         rango_start = self.date_start.strftime('%Y/%m/%d')
         rango_end = self.date_end.strftime('%Y/%m/%d')
         #colocar un titulo que diga SEBIGUS SRL en la primera fila centrado y en negrita de la a columna A a la E
-        worksheet.merge_range(0, 0, 0, 4, 'SEBIGUS SRL', formato_encabezado)
+        worksheet.merge_range(0, 0, 0, 7, 'SEBIGUS SRL', formato_encabezado)
         #colocar un subtitulo que diga REPORTE DE ASISTENCIA en la segunda fila centrado y en negrita de la a columna A a la E
-        worksheet.merge_range(1, 0, 1, 4, 'REPORTE DE ASISTENCIA', formato_encabezado)
+        worksheet.merge_range(1, 0, 1, 7, 'REPORTE DE ASISTENCIA', formato_encabezado)
         #colocar un subtitulo que diga el rango de fechas en la tercera fila centrado y en negrita de la a columna A a la E
-        worksheet.merge_range(2, 0, 2, 4, f'Rango de Fechas: {rango_start} a {rango_end}', formato_encabezado)
+        worksheet.merge_range(2, 0, 2, 7, f'Rango de Fechas: {rango_start} a {rango_end}', formato_encabezado)
         # Escribir encabezados
         header_row = 4
         worksheet.write(header_row, 0, 'EMPLEADO', formato_encabezado)
         worksheet.write(header_row, 1, 'INGRESO', formato_encabezado)
         worksheet.write(header_row, 2, 'SALIDA', formato_encabezado)
-        worksheet.write(header_row, 3, 'TIPO DE EMPLEADO', formato_encabezado)
-        worksheet.write(header_row, 4, 'TURNO ASIGNADO', formato_encabezado)
+        worksheet.write(header_row, 3, 'HORAS TRABAJADAS', formato_encabezado)
+        worksheet.write(header_row, 4, 'HORAS AL 50%/EXTRA', formato_encabezado)
+        worksheet.write(header_row, 5, 'HORAS AL 100%/FERIADOS/SABADOS', formato_encabezado)
+        worksheet.write(header_row, 6, 'TIPO DE EMPLEADO', formato_encabezado)
+        worksheet.write(header_row, 7, 'TURNO ASIGNADO', formato_encabezado)
         # Buscar facturas en el rango de fechas
         domain = [
             ('check_in', '>=', self.date_start),
@@ -109,7 +100,7 @@ class HrReportAttendanceWizard(models.TransientModel):
             emp = attendance.employee_id
             # Cuando cambia el empleado, insertar “título de empleado”
             if not current_emp or emp.id != current_emp.id:
-                worksheet.merge_range(row, 0, row, 5, emp.name or '—', fmt_emp)
+                worksheet.merge_range(row, 0, row, 7, emp.name or '—', fmt_emp)
                 row += 1
                 current_emp = emp
             # Fechas formateadas
@@ -118,7 +109,7 @@ class HrReportAttendanceWizard(models.TransientModel):
             tipo_empleado = 'Empleado' if attendance.employee_type == 'employee' else 'Eventual' if attendance.employee_type == 'eventual' else ''
             turno_asignado = 'Día' if attendance.employee_type_shift == 'day' else 'Noche' if attendance.employee_type_shift == 'night' else ''
             #como colocar una linea separacion entre empleados
-            worksheet.write(row, 0, attendance.employee_id.name, formato_celdas_derecha)
+            worksheet.write(row, 0, attendance.employee_id.name, formato_celdas_izquierda)
             worksheet.write(row, 1, ingreso, formato_celdas_derecha)
             worksheet.write(row, 2, salida, formato_celdas_derecha)
             worksheet.write(row, 3, tipo_empleado, formato_celdas_izquierda)
