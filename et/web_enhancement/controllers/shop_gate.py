0cc11ea9100dfffa8c036ea5803b103ebc06b7bd
ab-iceberg <rbmaldonado.1scz@gmail.com>
2025-10-16 17:43:59 -0300

    feat: se agrego el reporte de excel para el reporte de asistencia del empleado

--- et/web_enhancement/controllers/shop_gate.py
+++ et/web_enhancement/controllers/shop_gate.py
@@ -8,11 +8,10 @@ class ShopGate(http.Controller):
     def shop_gate(self, **post):
         msg = None
         if request.httprequest.method == 'POST':
-            pwd_cfg = '123456' #request.env['ir.config_parameter'].sudo().get_param('website.shop_gate_password') or ''
+            pwd_cfg = request.env['ir.config_parameter'].sudo().get_param('website.shop_gate_password') or ''
             if (post.get('password') or '') == pwd_cfg:
                 request.session['shop_gate_ok'] = True
-                request.session['shop_gate_until'] = time.time() + 60
+                request.session['shop_gate_until'] = time.time() + (5 * 60 * 60)  # 5 horas
                 return request.redirect('/shop')
             msg = _("Contraseña incorrecta. Intenta de nuevo.")
-
         return request.render('web_enhancement.shop_gate', {'msg': msg})
--- et/web_enhancement/models/res_config_settings.py
+++ et/web_enhancement/models/res_config_settings.py
@@ -5,13 +5,17 @@ class ResConfigSettings(models.TransientModel):
     _description = 'Configuracion de sistemas'
 
     sheet_drive_folder_path = fields.Char(string="ID de Carpeta en Drive", help="ID de la carpeta en Drive Padre de las imagenes", required=True, default="123456789")
+    shop_gate_password = fields.Char(string="Contraseña de acceso a la tienda", help="Contraseña para acceder a la tienda", required=True, default="odoo2024")
 
     def set_values(self):
         super(ResConfigSettings, self).set_values()
         self.env['ir.config_parameter'].sudo().set_param('web_enhancement.sheet_drive_folder_path', self.sheet_drive_folder_path)
+        self.env['ir.config_parameter'].sudo().set_param('web_enhancement.shop_gate_password', self.shop_gate_password)
+
     def get_values(self):
         res = super(ResConfigSettings, self).get_values()
         res.update({
             'sheet_drive_folder_path': self.env['ir.config_parameter'].sudo().get_param('web_enhancement.sheet_drive_folder_path', default="123456789"),
+            'shop_gate_password': self.env['ir.config_parameter'].sudo().get_param('web_enhancement.shop_gate_password', default="odoo2024"),
         })
         return res
