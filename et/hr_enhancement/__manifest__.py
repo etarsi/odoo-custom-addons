dfab64f15d9c3ef9944bdc17b0b8770f6749861a
ab-iceberg <rbmaldonado.1scz@gmail.com>
2025-10-21 16:58:12 -0300

    feat: se agrego cambios para rrhh para el reporte de solicitudes

--- et/hr_enhancement/__manifest__.py
+++ et/hr_enhancement/__manifest__.py
@@ -24,11 +24,11 @@
 
     # always loaded
     'data': [
+        'data/res_groups_data.xml',
         'security/ir.model.access.csv',
         'security/hr_enhancement_groups.xml',
         'security/hr_enhancement_rules.xml',
         #Ingreso de datos por defecto
-        'data/res_groups_data.xml',
         'data/hr_license_type_data.xml',
         'data/action_hr_payroll_salary.xml',
         #Vistas
