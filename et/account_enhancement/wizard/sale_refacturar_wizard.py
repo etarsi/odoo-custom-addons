a905f64121e7697b2fdeec49b62e65639cf46a81
ab-iceberg <rbmaldonado.1scz@gmail.com>
2025-10-22 13:12:45 -0300

    feat: se agrego cambios para rrhh para el reporte de solicitudes

--- et/account_enhancement/wizard/sale_refacturar_wizard.py
+++ et/account_enhancement/wizard/sale_refacturar_wizard.py
@@ -29,6 +29,17 @@ class SaleRefacturarWizard(models.TransientModel):
             # pricelist_id vacío (None) por requerimiento
         })
         return res
+    
+    @api.onchange('company_id')
+    def _onchange_company_id(self):
+        if self.company_id:
+            domain = []
+            if self.company_id.id == '1':  # Producción B
+                self.condicion_m2m_id = False
+                domain = [('name', '=', 'Tipo 3')]
+                return {'domain': {'condicion_m2m_id': domain}}
+            else:
+                return False
 
     def action_confirm(self):
         self.ensure_one()
