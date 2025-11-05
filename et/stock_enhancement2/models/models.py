8fb6b6e4632ac85fca2044aca643c8cf6074fd27
unknown <tarsitano.ezequiel@hotmail.com>
2025-11-05 13:55:21 -0300

    fix domain

--- et/stock_enhancement2/models/models.py
+++ et/stock_enhancement2/models/models.py
@@ -184,7 +184,7 @@ class StockPickingInherit(models.Model):
                     else:
                         raise UserError(f'El producto {move.product_id.name} no tiene definido un código interno (default_code).')
 
-                    stock_move_erp_RESERV = self.env['stock.moves.erp'].search([('product_id.default_code', '=', search_code), ('type', '=', 'reserve')], limit=1)
+                    stock_move_erp_RESERV = self.env['stock.moves.erp'].search([('product_id.default_code', '=', search_code), ('type', '=', 'reserve'), ('sale_line_id', '=', move.sale_line_id.id)], limit=1)
                     stock_move_erp_PREPAR = self.env['stock.moves.erp'].search([('product_id.default_code', '=', search_code), ('picking_id', '=', record.id), ('type', '=', 'preparation')], limit=1)
 
                     stock_move_erp_RESERV.quantity_delivered += move.quantity_done
