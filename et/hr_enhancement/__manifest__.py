92509789003cba9c2c4e6272b1f37af89eb1a6c6
ab-iceberg <rbmaldonado.1scz@gmail.com>
2025-11-06 15:42:38 -0300

    feat: se agrego una mejora en el evento de crear asistencia

--- et/hr_enhancement/__manifest__.py
+++ et/hr_enhancement/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['hr', 'base', 'hr_attendance'],
+    'depends': ['hr', 'base', 'hr_attendance', 'web', 'mail'],
 
     # always loaded
     'data': [
