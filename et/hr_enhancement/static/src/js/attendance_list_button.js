985623464ef133515653c6369e9854a9e4e5adf8
ab-iceberg <rbmaldonado.1scz@gmail.com>
2025-11-10 10:52:43 -0300

    feat: se agrego en la vista de recursos humanos un modal para crear asistencia

--- et/hr_enhancement/static/src/js/attendance_list_button.js
+++ et/hr_enhancement/static/src/js/attendance_list_button.js
@@ -5,25 +5,7 @@ odoo.define('hr_enhancement.tree_button', function (require) {
   var ListView = require('web.ListView'); 
   var viewRegistry = require('web.view_registry'); 
 
-  function hrAttendance() {
-    if (this.$buttons) {
-        var self = this;
-        this._({
-            model: "sale",
-            method: "action_open_sale_order",
-            args: [false, {new_order: true}],
-            context: {calendar_summary_id: calendar_summary_id || false},
-        }).then(function (action) {
-            self.$buttons.on("click", ".o_button_new_sale_order", function () {
-                self.do_action(action);
-            });
-        });
-    }
-}
-
   var AttendanceListController = ListController.extend({ 
-    buttons_template: 'hr_enhancement.buttons', 
-
     events: _.extend({}, ListController.prototype.events, { 
       'click .open_wizard_button': '_openAttendanceWizard', 
     }), 
