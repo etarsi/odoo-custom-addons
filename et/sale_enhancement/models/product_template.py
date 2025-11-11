40a7cbaef8c0e82e08f7250ce3c87cb18cc9043b
ab-iceberg <rbmaldonado.1scz@gmail.com>
2025-11-11 10:38:01 -0300

    feat: se agrego el monto de productos template de action

--- et/sale_enhancement/models/product_template.py
+++ et/sale_enhancement/models/product_template.py
@@ -1,4 +1,6 @@
 from odoo import models, fields, api, _
+import logging
+_logger = logging.getLogger(__name__)
 
 RUBRO_COMPANY_MAPPING = {
     'JUGUETES': 3,
@@ -31,10 +33,14 @@ class ProductTemplateInherit(models.Model):
         categorias = self.env['product.category'].search([('parent_id', '=', False)])
         if categorias:
             for categoria in categorias:
+                _logger.info(f"Checking category {categoria.name} with ID {categoria.id} against product category {self.categ_id.name} with parent ID {self.categ_id.parent_id}")
                 if self.categ_id.parent_id == categoria.id:
+                    _logger.info(f"Category match found: {categoria.name}")
                     rubro = categoria.name.upper().strip()
                     if rubro in RUBRO_COMPANY_MAPPING:
+                        _logger.info(f"Rubro found in mapping: {rubro}")
                         company_id = RUBRO_COMPANY_MAPPING[rubro]
                         company_ids.append(company_id)
         if company_ids:
+            _logger.info(f"COMPAÑIAS INGRESO {company_ids}")
             self.company_ids = [(6, 0, list(set(company_ids)))]
