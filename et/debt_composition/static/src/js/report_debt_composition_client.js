f396ae876541705d36c507847a895282b042563a
ab-iceberg <rbmaldonado.1scz@gmail.com>
2025-11-12 08:57:19 -0300

    feat: se modifico el metodo de actualizar la composicion de deuda

--- et/debt_composition/static/src/js/report_debt_composition_client.js
+++ et/debt_composition/static/src/js/report_debt_composition_client.js
@@ -2,24 +2,37 @@ odoo.define('debt_composition.rdcc_reload_patch', function (require) {
     "use strict";
 
     const ListController = require('web.ListController');
-
     const _reload = ListController.prototype.reload;
 
     ListController.include({
+        init: function () {
+            this._super.apply(this, arguments);
+            this.__rdccRefreshing = false;
+        },
         reload: function () {
             if (this.modelName !== 'report.debt.composition.client') {
                 return _reload.apply(this, arguments);
             }
+            if (this.__rdccRefreshing) {
+                return _reload.apply(this, arguments);
+            }
+            this.__rdccRefreshing = true;
+
             const args = arguments;
             const self = this;
+
             return this._rpc({
                 model: 'report.debt.composition.client',
                 method: 'action_refresh_sql',
-                args: [],        
-            }).then(
-                function () { return _reload.apply(self, args); },
-                function () { return _reload.apply(self, args); }
-            );
+                args: [],
+                kwargs: {},
+            }).then(function () {
+                self.__rdccRefreshing = false;
+                return _reload.apply(self, args);
+            }, function () {
+                self.__rdccRefreshing = false;
+                return _reload.apply(self, args);
+            });
         },
     });
 });
