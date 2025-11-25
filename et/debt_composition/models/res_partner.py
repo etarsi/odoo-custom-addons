c8e0f819e06c7351aa777ca60921859b102887f0
ab-iceberg <rbmaldonado.1scz@gmail.com>
2025-11-25 13:34:35 -0300

    feat: se agrego la composicion de deuda del cliente por compañia

--- et/debt_composition/models/res_partner.py
+++ et/debt_composition/models/res_partner.py
@@ -19,8 +19,13 @@ class ResPartnerInherit(models.Model):
             partner_id=partner_id,
             company_ids=company_ids,
         )
+        data=[]
         lines = self.env['report.debt.composition.client.company'].search([('partner', '=', partner_id), ('company_id', 'in', company_ids)])
         if lines:
+            #modificar la fecha por el formato d/m/Y
+            for line in lines:
+                if line.date:
+                    line.date = line.date.strftime('%d/%m/%Y')
             return self.env.ref(
                 'debt_composition.report_debt_composition_client_company_pdf'
             ).report_action(lines)
