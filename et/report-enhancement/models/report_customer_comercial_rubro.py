f7ea9d1eddaa381c1ea5337bdf6910660f4ea7e9
ab-iceberg <rbmaldonado.1scz@gmail.com>
2025-11-28 11:03:21 -0300

    feat: se añadio el nuevo report_customer de rubro de comercial

--- et/report-enhancement/models/report_customer_comercial_rubro.py
+++ et/report-enhancement/models/report_customer_comercial_rubro.py
@@ -1,7 +1,7 @@
 # models/report_customer_rubro.py
 from odoo import models, fields, tools
 
-class ReportCustomerCommercialRubro(models.Model):
+class ReportCustomerComercialRubro(models.Model):
     _name = 'report.customer.comercial.rubro'
     _description = 'Facturación por cliente / comercial y rubro'
     _auto = False
@@ -31,14 +31,8 @@ class ReportCustomerCommercialRubro(models.Model):
                     am.invoice_user_id AS comercial_id,
                     parent_categ.id AS rubro_id,
                     am.currency_id AS currency_id,
-                    SUM(
-                        CASE
-                            -- Si es NC, damos vuelta el signo
-                            WHEN am.move_type = 'out_refund'
-                                THEN -aml.price_subtotal
-                            ELSE aml.price_subtotal
-                        END
-                    ) AS amount
+                    -- Monto del rubro (solo facturas de clientes)
+                    SUM(aml.price_subtotal) AS amount
                 FROM account_move_line aml
                 JOIN account_move am
                     ON aml.move_id = am.id
@@ -53,8 +47,8 @@ class ReportCustomerCommercialRubro(models.Model):
                     ON parent_categ.id = COALESCE(c.parent_id, c.id)
                 WHERE
                     am.state = 'posted'
-                    AND am.move_type IN ('out_invoice', 'out_refund')
-                    AND aml.display_type IS NULL   -- sin notas/secciones
+                    AND am.move_type = 'out_invoice'
+                    AND aml.display_type IS NULL
                 GROUP BY
                     am.company_id,
                     am.invoice_date,
@@ -64,3 +58,4 @@ class ReportCustomerCommercialRubro(models.Model):
                     am.currency_id
             )
         """ % self._table)
+
