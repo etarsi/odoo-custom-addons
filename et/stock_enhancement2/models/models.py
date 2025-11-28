33fa222dbe9f94ce11b695ee3ea7009ab1928b14
etarsi <tarsitano.ezquiel@hotmail.com>
2025-11-28 12:46:05 -0300

    fix: mark as delivered

--- et/stock_enhancement2/models/models.py
+++ et/stock_enhancement2/models/models.py
@@ -207,7 +207,7 @@ class StockPickingInherit(models.Model):
                     vals['type'] = 'delivery'
 
                     vals_list.append(vals)
-            self.env['stock.moves.erp'].create(vals_list)
+            # self.env['stock.moves.erp'].create(vals_list)
 
     ### PICKING
 
@@ -223,7 +223,7 @@ class StockPickingInherit(models.Model):
                 if record.picking_type_code == 'outgoing':
                     move_type = 'ENTREGA'
                     record.action_create_product_moves(move_type)
-                    # record.consume_stock()
+                    record.consume_stock()
                     record.action_write_tms_stock_picking()
 
     def update_availability(self):
