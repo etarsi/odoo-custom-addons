059229df550b0de4233e77e159c2cea4b50e16aa
ab-iceberg <rbmaldonado.1scz@gmail.com>
2025-12-22 12:56:18 -0300

    feat: se añadio el cliente proveedor para la configuracion

--- et/account_enhancement/models/res_partner.py
+++ et/account_enhancement/models/res_partner.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from odoo import models, api, fields, _
+from odoo import models, fields, _
 from collections import OrderedDict
 from dateutil.relativedelta import relativedelta
 from odoo.exceptions import AccessError, UserError, ValidationError
