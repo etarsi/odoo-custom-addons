33e9407d25f44c30249a70825b655eaf98231b3d
ab-iceberg <rbmaldonado.1scz@gmail.com>
2025-12-26 12:45:29 -0300

    feat: se añadio las modificaciones del modelo de factura para los clientes

--- et/account_enhancement/models/models.py
+++ et/account_enhancement/models/models.py
@@ -111,11 +111,13 @@ class AccountMoveInherit(models.Model):
             email_values_cliente = {
                 "email_from": f"Sebigus <{move.company_id.email}>",
                 "email_to": move.partner_id.email,
+                "subject": f"Factura {move.name}" if move.company_id.id != 1 else f"Cotización {move.name}",
                 "attachment_ids": [(4, attachment.id)],
             }
             email_values_user = {
                 "email_from": f"Sebigus <{move.company_id.email}>",
                 "email_to": move.invoice_user_id.partner_id.email,
+                "subject": f"Factura {move.name}" if move.company_id.id != 1 else f"Cotización {move.name}",
                 "attachment_ids": [(4, attachment.id)],
             }
 
