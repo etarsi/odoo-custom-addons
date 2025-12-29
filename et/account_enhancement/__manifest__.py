8e58858d8e8875933388e82f574139679adb5d48
ab-iceberg <rbmaldonado.1scz@gmail.com>
2025-12-29 17:13:04 -0300

    feat: se añadio el nuevo modal para subir el archivo TXT para importar los impuestos IIBB

--- et/account_enhancement/__manifest__.py
+++ et/account_enhancement/__manifest__.py
@@ -30,7 +30,7 @@
         ],
     },
     # any module necessary for this one to work correctly
-    'depends': ['base', 'account', 'account_check_printing', 'account_payment_group', 'l10n_latam_check', 'l10n_ar', 'l10n_ar_reports','l10n_ar_afipws_fe', 'mail', 'sale', 'product', 'sale_enhancement', 'l10n_ar_ux', 'account_financial_report'],
+    'depends': ['base', 'account', 'account_check_printing', 'account_payment_group', 'l10n_latam_check', 'l10n_ar', 'l10n_ar_reports','l10n_ar_afipws_fe', 'mail', 'sale', 'product', 'sale_enhancement', 'l10n_ar_ux', 'account_financial_report', 'l10n_ar_account_withholding'],
 
     # always loaded
     'data': [
