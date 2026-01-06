b61a95e52d53e094a90412843ad96848a0236794
ab-iceberg <rbmaldonado.1scz@gmail.com>
2026-01-06 11:51:28 -0300

    feat: se añadio mejoras en rrhh para los empleados fijos

--- et/account_enhancement/models/account_debit_note.py
+++ et/account_enhancement/models/account_debit_note.py
@@ -22,3 +22,28 @@ class AccountDebitNote(models.TransientModel):
             _logger.info("Copied product lines for debit note from move %s", move.name)
             _logger.info("Default values: %s", default_values)
         return default_values
+    
+    def create_debit(self):
+        res = super().create_debit()
+
+        Move = self.env["account.move"]
+        new_moves = Move.browse()
+
+        # Caso 1: se creó 1
+        if res.get("res_id"):
+            new_moves = Move.browse(res["res_id"])
+
+        # Caso 2: se crearon varios -> viene domain [('id','in',[...])]
+        else:
+            for d in (res.get("domain") or []):
+                if isinstance(d, (list, tuple)) and len(d) == 3 and d[0] == "id" and d[1] == "in":
+                    new_moves = Move.browse(d[2])
+                    break
+        _logger.info("New debit note moves created: %s", new_moves.mapped("name"))
+        _logger.info("Recomputing latam document types for debit notes")
+        # Recomputar tipo de doc + onchange para todos
+        for m in new_moves:
+            m._compute_l10n_latam_document_type()
+            m._onchange_l10n_latam_document_type_id()
+
+        return res
