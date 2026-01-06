66a29de5f8d6177dd2b80da8b3862df894125318
ab-iceberg <rbmaldonado.1scz@gmail.com>
2026-01-06 11:13:14 -0300

    feat: se añadio mejoras en rrhh para los empleados fijos

--- et/hr_enhancement/models/hr_attendance.py
+++ et/hr_enhancement/models/hr_attendance.py
@@ -92,60 +92,65 @@ class HrAttendance(models.Model):
                 if att.employee_id.employee_type == 'eventual':
                     att.event_hours_employee_eventual(schedule)
                 elif att.employee_id.employee_type == 'employee':
-                    att.event_hours_employee(schedule)
+                    att.event_hours_employee()
 
-    def event_hours_employee(self, schedule):
+    def event_hours_employee(self):
         for att in self:
-            start_t = self._float_to_time(schedule.hour_start + 3)
-            end_t   = self._float_to_time(schedule.hour_end + 3)
-            day = att.check_in.date()
-            start_dt = datetime.combine(day, start_t)
-            end_dt   = datetime.combine(day, end_t)
-            # REGLA SOLICITADA PARA check_in
-            ci_floor = att.check_in.replace(minute=0, second=0, microsecond=0)
-            eff_check_in = max(ci_floor, start_dt)
-            eff_check_out = att.check_out
-            #verificar si hay hr holiday custom
-            holiday = self.env['hr.holiday.custom'].search([('date', '=', day)], limit=1)
-            #sacar los dias
-            dow = day.weekday()
-            if dow == 6 or holiday:
-                eff_check_in = att.check_in
-                total_secs = max(0.0, (eff_check_out - eff_check_in).total_seconds())
-                att.holiday_hours = float(self._round_30_up_to_int_hours(total_secs))
+            if att.employee_id.work_schedule_id:
+                start_t = self._float_to_time(att.employee_id.work_schedule_id.hour_start + 3)
+                end_t   = self._float_to_time(att.employee_id.work_schedule_id.hour_end + 3)
+                day = att.check_in.date()
+                start_dt = datetime.combine(day, start_t)
+                end_dt   = datetime.combine(day, end_t)
+                # REGLA SOLICITADA PARA check_in
+                ci_floor = att.check_in.replace(minute=0, second=0, microsecond=0)
+                eff_check_in = max(ci_floor, start_dt)
+                eff_check_out = att.check_out
+                #verificar si hay hr holiday custom
+                holiday = self.env['hr.holiday.custom'].search([('date', '=', day)], limit=1)
+                #sacar los dias
+                dow = day.weekday()
+                if dow == 6 or holiday:
+                    eff_check_in = att.check_in
+                    total_secs = max(0.0, (eff_check_out - eff_check_in).total_seconds())
+                    att.holiday_hours = float(self._round_30_up_to_int_hours(total_secs))
+                    att.worked_hours = 0.0
+                    att.overtime = 0.0
+                    att.hours_late = 0.0
+                    continue
+
+                scheduled_overlap_secs = self._overlap_seconds(
+                    eff_check_in, eff_check_out, start_dt, end_dt
+                )
+                base_hours_raw = scheduled_overlap_secs / 3600.0
+
+                if dow == 5:  # Sábado
+                    start_t = self._float_to_time(att.employee_id.work_schedule_id.hour_start + 4) #comienzan 1 hora más tarde
+                    start_dt = datetime.combine(day, start_t)
+                    sat_end_t  = self._float_to_time(15.0) # terminan a las 12:00 BA
+                    sat_end_dt = datetime.combine(day, sat_end_t)
+                    base_secs_raw = self._overlap_seconds(
+                        eff_check_in, eff_check_out, start_dt, sat_end_dt
+                    )
+                    base_hours_raw = base_secs_raw / 3600.0
+                    holiday_base = 0.0
+                    if eff_check_out > sat_end_dt:
+                        holiday_base = max(0.0, (eff_check_out - max(eff_check_in, sat_end_dt)).total_seconds())
+                    att.holiday_hours += float(self._round_30_up_to_int_hours(holiday_base)) + base_hours_raw
+                    att.worked_hours = 0.00
+                    att.overtime = 0.0
+                else:
+                    # ===== Lunes a Viernes (tu lógica original) =====
+                    base_hours = base_hours_raw
+                    over_time_base = 0.0
+                    if eff_check_out > end_dt:
+                        over_time_base = max(0.0, (eff_check_out - max(eff_check_in, end_dt)).total_seconds())
+                    att.overtime += float(self._round_30_up_to_int_hours(over_time_base))
+                    att.worked_hours = base_hours
+            else:
                 att.worked_hours = 0.0
                 att.overtime = 0.0
-                att.hours_late = 0.0
-                continue
-
-            scheduled_overlap_secs = self._overlap_seconds(
-                eff_check_in, eff_check_out, start_dt, end_dt
-            )
-            base_hours_raw = scheduled_overlap_secs / 3600.0
-
-            if dow == 5:  # Sábado
-                start_t = self._float_to_time(schedule.hour_start + 4) #comienzan 1 hora más tarde
-                start_dt = datetime.combine(day, start_t)
-                sat_end_t  = self._float_to_time(15.0) # terminan a las 12:00 BA
-                sat_end_dt = datetime.combine(day, sat_end_t)
-                base_secs_raw = self._overlap_seconds(
-                    eff_check_in, eff_check_out, start_dt, sat_end_dt
-                )
-                base_hours_raw = base_secs_raw / 3600.0
-                holiday_base = 0.0
-                if eff_check_out > sat_end_dt:
-                    holiday_base = max(0.0, (eff_check_out - max(eff_check_in, sat_end_dt)).total_seconds())
-                att.holiday_hours += float(self._round_30_up_to_int_hours(holiday_base)) + base_hours_raw
-                att.worked_hours = 0.00
-                att.overtime = 0.0
-            else:
-                # ===== Lunes a Viernes (tu lógica original) =====
-                base_hours = base_hours_raw
-                over_time_base = 0.0
-                if eff_check_out > end_dt:
-                    over_time_base = max(0.0, (eff_check_out - max(eff_check_in, end_dt)).total_seconds())
-                att.overtime += float(self._round_30_up_to_int_hours(over_time_base))
-                att.worked_hours = base_hours
+                att.holiday_hours = 0.0
 
     def event_hours_employee_eventual(self, schedule):
         for att in self:
