cf00e5c4a06223dc13dcd6ce0ae9e0735c9ce072
ab-iceberg <rbmaldonado.1scz@gmail.com>
2026-01-07 10:47:44 -0300

    feat: se añadio las correciones de debito de la nota de facturacion

--- et/account_enhancement/models/__init__.py
+++ et/account_enhancement/models/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
 
 from . import models, account_vat_ledger, debt_composition, account_payment, account_payment_group
-from . import report_factura_proveedor, res_partner, account_debit_note
+from . import report_factura_proveedor, res_partner
+from . import account_debit_note
