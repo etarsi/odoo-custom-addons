ddaad53f449e7d9cfd820caa2beb76de377f4903
ab-iceberg <rbmaldonado.1scz@gmail.com>
2026-01-08 14:02:14 -0300

    feat: se añadio el reporte de libro mayor para el reporte generar

--- et/account_enhancement/__manifest__.py
+++ et/account_enhancement/__manifest__.py
@@ -42,6 +42,8 @@
         'views/account_payment_group_suprimir_wizard_views.xml',
         'views/account_import_afip_facprov_wizard_views.xml',
         'views/import_afip_impuesto_bruto_wizard_views.xml',
+        'views/report_button_custom_imprimir.xml',
+        'data/report_general_ledger.xml',
         'data/action_payment_group.xml',
         #Reportes
         'wizard/generar_factura_wizard_views.xml',
