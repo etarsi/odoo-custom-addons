49a761dd3a0e840c169444cac8e80fb3d53424ae
ab-iceberg <rbmaldonado.1scz@gmail.com>
2026-01-09 12:45:48 -0300

    feat: se añadio el reporte de libro mayor para el reporte generar

--- et/account_enhancement/static/src/js/afr_bind_export_excel.js
+++ et/account_enhancement/static/src/js/afr_bind_export_excel.js
@@ -1,29 +1,12 @@
 odoo.define("account_enhancement.afr_bind_export_excel", function (require) {
     "use strict";
 
-    const AFRAction = require("account_financial_report.client_action");
+    console.log("[CP ALERT] cargado");
 
-    AFRAction.include({
-        start: function () {
-            return this._super.apply(this, arguments).then(() => {
-                // Control Panel DOM
-                const $cp = this._controlPanelWrapper && this._controlPanelWrapper.$el;
-                if (!$cp) return;
-                console.log("AFRAction: bind export to excel button");
-                console.log($cp);
-                console.log(this);
-
-                // Evitar duplicar handlers
-                $cp.off("click", ".o_report_export_excel");
-
-                // Click => export xlsx del AFR
-                $cp.on("click", ".o_report_export_excel", (ev) => {
-                    ev.preventDefault();
-                    if (typeof this.on_click_export === "function") {
-                        return this.on_click_export();
-                    }
-                });
-            });
-        },
+    // Delegación global: funciona aunque el control panel se re-renderice
+    $(document).on("click", ".o_report_export_excel", function (ev) {
+        ev.preventDefault();
+        alert("Hola! Click en Exportar Excel");
+        console.log("[CP ALERT] click en Exportar Excel");
     });
 });
