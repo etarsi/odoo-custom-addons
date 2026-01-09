986f86bb557df7aa314b4c653c5ed29d39c7c745
ab-iceberg <rbmaldonado.1scz@gmail.com>
2026-01-09 10:20:49 -0300

    feat: se añadio el reporte de libro mayor para el reporte generar

--- et/account_enhancement/static/src/js/client_action.js
+++ et/account_enhancement/static/src/js/client_action.js
@@ -1,30 +1,26 @@
-odoo.define("account_enhancement.client_action_enhancement", function (require) {
+odoo.define("account_enhancement.afr_override", function (require) {
     "use strict";
 
     var ReportAction = require("report.client_action");
+    var action_registry = require("web.action_registry");
     var core = require("web.core");
-
     var QWeb = core.qweb;
 
-    const AFRReportAction = ReportAction.extend({
+    const MyAFRReportAction = ReportAction.extend({
         start: function () {
+            console.log("[MY AFR] start()", this);  // <- este log debe aparecer sí o sí si se ejecuta la acción
+
             return this._super.apply(this, arguments).then(() => {
+                // Render de TU template (o el original si querés)
                 this.$buttons = $(
-                    QWeb.render(
-                        "account_financial_report.client_action.ControlButtons",
-                        {}
-                    )
+                    QWeb.render("account_financial_report.client_action.ControlButtons", {})
+                    // o tu template: QWeb.render("tu_modulo.ControlButtons", {})
                 );
-                console.log("Buttons:", this.$buttons);
-                console.log("Report Name:", this.report_name);
-                console.log("Report File:", this.report_file);
-                this.$buttons.on("click", ".o_report_print", this.on_click_print);
-                this.$buttons.on("click", ".o_report_export", this.on_click_export);
 
-                this.controlPanelProps.cp_content = {
-                    $buttons: this.$buttons,
-                };
+                this.$buttons.on("click", ".o_report_print", this.on_click_print.bind(this));
+                this.$buttons.on("click", ".o_report_export", this.on_click_export.bind(this));
 
+                this.controlPanelProps.cp_content = { $buttons: this.$buttons };
                 this._controlPanelWrapper.update(this.controlPanelProps);
             });
         },
@@ -42,20 +38,15 @@ odoo.define("account_enhancement.client_action_enhancement", function (require) 
             return this.do_action(action);
         },
 
-        /**
-         * @param {String} str
-         * @returns {String}
-         */
         _get_xlsx_name: function (str) {
-            if (!_.isString(str)) {
-                return str;
-            }
+            if (!_.isString(str)) return str;
             const parts = str.split(".");
             return `a_f_r.report_${parts[parts.length - 1]}_xlsx`;
         },
     });
 
-    core.action_registry.add("account_enhancement.client_action_enhancement", AFRReportAction);
+    // Clave: registrar con el MISMO tag para reemplazar al original
+    action_registry.add("account_financial_report.client_action", MyAFRReportAction);
 
-    return AFRReportAction;
+    return MyAFRReportAction;
 });
