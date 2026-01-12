4e254f932c0d5dcae0243033bd396aa3645d7b69
ab-iceberg <rbmaldonado.1scz@gmail.com>
2026-01-12 09:34:20 -0300

    fix: se añadio unas mejoras al control de panel extendido

--- et/account_enhancement/static/src/js/afr_export_excel.js
+++ et/account_enhancement/static/src/js/afr_export_excel.js
@@ -3,23 +3,37 @@ odoo.define("account_enhancement.afr_export_xlsx", function (require) {
 
     console.log("[XLSX] afr_export_xlsx cargado");
 
-    const AFRAction = require("account_financial_report.client_action"); // tu require actual
+    const AFRAction = require("account_financial_report.client_action");
 
     AFRAction.include({
-        // en vez de _.extend(...)
-        events: Object.assign({}, AFRAction.prototype.events, {
-            "click .o_report_export_excel": "_onExportExcel",
-        }),
+        start: function () {
+            const res = this._super.apply(this, arguments);
+
+            return Promise.resolve(res).then(() => {
+                if (!this.$buttons) {
+                    console.warn("[XLSX] No existe this.$buttons, no puedo bindear.");
+                    return;
+                }
+
+                // Verificación: si esto da 0, tu botón NO está en el template correcto
+                console.log("[XLSX] botones encontrados:", this.$buttons.find(".o_report_export_excel").length);
+
+                this.$buttons.off("click", ".o_report_export_excel");
+                this.$buttons.on("click", ".o_report_export_excel", this._onExportExcel.bind(this));
+            });
+        },
 
         _onExportExcel: function (ev) {
             ev.preventDefault();
             console.log("[XLSX] click export");
 
+            // OJO: acá tenés que llamar el XLSX real (ver punto 3)
             return this.do_action({
                 type: "ir.actions.report",
                 report_type: "xlsx",
-                report_name: "account_enhancement.report_general_ledger_xlsx",
+                report_name: "a_f_r_report_general_ledger_xlsx",
                 data: { options: this.report_options || {} },
+                context: this.context,
             });
         },
     });
