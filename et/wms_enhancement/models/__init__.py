fed16f6e2fd9db6a352f74b6beb0984ba5d90f7a
etarsi <tarsitano.ezquiel@hotmail.com>
2026-01-18 17:00:48 -0300

    fix: init reference

--- et/wms_enhancement/models/__init__.py
+++ et/wms_enhancement/models/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from . import wms_config, wms_history, wms_stats, wms_stock, wms_stock_resume, wms_stock_location, wms_task, wms_inventory_adjustment, wms_transfer
+from . import wms_config, wms_history, wms_stats, wms_stock_resume, wms_stock_location, wms_task, wms_inventory_adjustment, wms_transfer
