4183762ae96317e1849e83c46f4c1674740410c5
etarsi <tarsitano.ezquiel@hotmail.com>
2026-01-20 13:50:15 -0300

    fix

--- et/wms_enhancement/models/__init__.py
+++ et/wms_enhancement/models/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
 
 # from . import wms_config, wms_history, wms_stats, wms_stock_resume, wms_stock_location, wms_task, wms_inventory_adjustment, wms_transfer
-from . import wms_transfer, wms_task
+# from . import wms_transfer, wms_task
+from . import wms_area
