962fee57b81f1751291d8f0588f8bf842e3722a5
etarsi <tarsitano.ezquiel@hotmail.com>
2026-01-24 16:08:29 -0300

    new fields

--- et/wms_enhancement/models/wms_task.py
+++ et/wms_enhancement/models/wms_task.py
@@ -5,20 +5,43 @@ class WMSTask(models.Model):
     _name = 'wms.task'
 
     name = fields.Char()    
-    state = fields.Selection(string="Estado", selection=[
-        ('draft', 'Borrador'),
+    state_preparation = fields.Selection(string="Estado", selection=[
+        ('no', 'No aplica'),
+        ('preparation', 'En Preparación'),
+        ('control', 'Control'),
+        ('delivery', 'Entregado'),
+        ('cancel', 'Cancelado'),
+    ])
+    state_reception = fields.Selection(string="Estado", selection=[
+        ('no', 'No aplica'),
         ('pending', 'Pendiente'),
-        ('confirmed', 'Confirmado'),
-        ('process', 'En Proceso'),
-        ('done', 'Hecho')
+        ('inprocess', 'En Proceso'),
+        ('received', 'Ingresado'),
+        ('cancel', 'Cancelado'),
+    ])
+    state_return = fields.Selection(string="Estado", selection=[
+        ('no', 'No aplica'),
+        ('pending', 'Pendiente'),
+        ('inprocess', 'En Proceso'),
+        ('control', 'Control'),
+        ('')
+        ('cancel', 'Cancelado'),
+    ])
+    state_reception = fields.Selection(string="Estado", selection=[
+        ('no', 'No aplica'),
+        ('pending', 'Pendiente'),
+        ('control', 'Control'),
+        ('complete', 'Completado'),
+        ('cancel', 'Cancelado'),
     ])
     type = fields.Selection(string="Tipo", selection=[
         ('reception', 'Recepción'),
         ('preparation', 'Preparación'),
-        ('control', 'Control'),
+        ('return', 'Devolución'),
         ('undo', 'Desarmar'),
         ('fraction', 'Fraccionado'),
-        ('replenish', 'Reposición')
+        ('replenish', 'Reposición'),
+        ('relocate', 'Reubicación')
     ])
     priority = fields.Integer(string="Prioridad")
     assigned_user_id = fields.Many2one(string="Asignado a", comodel_name="res.users")
@@ -53,6 +76,29 @@ class WMSTask(models.Model):
     done_at = fields.Datetime(string="Fin")
 
 
+    def action_confirm(self):
+        for record in self:
+
+            if record.type == 'return':
+                
+                lines_to_relocate = []
+                lines_to_scrap = []
+
+                for line in record.line_ids:
+                    if line.is_broken:
+                        lines_to_scrap.append(line.product_id)
+
+                task_vals = {
+                    'type':'relocate',
+                    'priority':1,
+                    'transfer_id':record.transfer_id.id,
+                    
+                }
+
+
+                if lines_to_scrap:
+                    self.env['wms.task'].create(task_vals)
+
 
 class WMSTaskLine(models.Model):
     _name = 'wms.task.line'
@@ -63,3 +109,5 @@ class WMSTaskLine(models.Model):
     quantity = fields.Integer(string="Demanda")
     quantity_picked = fields.Integer(string="Cantidad pickeada")
     quantity_controlled = fields.Integer(string="Cantidad Controlada")
+
+    is_broken = fields.Boolean(string="¿Está roto?")
