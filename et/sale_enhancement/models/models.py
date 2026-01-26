798c8fa4fe066396715dde1f9f344a4ce335ed6e
ab-iceberg <rbmaldonado.1scz@gmail.com>
2026-01-26 09:49:29 -0300

    feat: se comento el codigo de confirmacion de pedidos de venta el metodo de crear la transferencia wms

--- et/sale_enhancement/models/models.py
+++ et/sale_enhancement/models/models.py
@@ -557,26 +557,27 @@ class SaleOrderInherit(models.Model):
                 'sale_id':record.id,
             }
 
-            transfer_id = self.env['wms.transfer'].create(transfer_vals)
+            # COMENTADO PARA QUE NO CAUSE ERROR AL CONFIRMAR PEDIDO SIN WMS (TITO)
 
-            transfer_lines_list = []
-            for line in record.order_line:
-                if line.product_id:
-                    transfer_line = {
-                        'transfer_id': transfer_id.id,
-                        'product_id': line.product_id.id,
-                        'state': 'pending',
-                        'invoice_state': 'no',
-                        'sale_line_id': line.id,
-                        'uxb': line.product_packaging_id.qty or False,
-                        'qty_demand': line.product_uom_qty or 0,
-                    }
+            #transfer_id = self.env['wms.transfer'].create(transfer_vals)
+            #transfer_lines_list = []
+            #for line in record.order_line:
+            #    if line.product_id:
+            #        transfer_line = {
+            #            'transfer_id': transfer_id.id,
+            #            'product_id': line.product_id.id,
+            #            'state': 'pending',
+            #            'invoice_state': 'no',
+            #            'sale_line_id': line.id,
+            #            'uxb': line.product_packaging_id.qty or False,
+            #            'qty_demand': line.product_uom_qty or 0,
+            #        }
 
-                    transfer_lines_list.append(transfer_line)
+            #        transfer_lines_list.append(transfer_line)
             
-            self.env['wms.transfer.line'].create(transfer_lines_list)
+            #self.env['wms.transfer.line'].create(transfer_lines_list)
 
-            record.transfer_id = transfer_id.id
+            #record.transfer_id = transfer_id.id
             
         return res
 
