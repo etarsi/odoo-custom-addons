8bf458a642ae386297aad0af9eba8f63bca6c3b8
ab-iceberg <rbmaldonado.1scz@gmail.com>
2026-01-26 17:49:33 -0300

    feat: se añadio las mejoras para facturacion sobre el name formado por el product_id

--- et/account_enhancement/models/models.py
+++ et/account_enhancement/models/models.py
@@ -673,7 +673,17 @@ class AccountMovelLineInherit(models.Model):
 
     lot_id = fields.Many2one('stock.production.lot', string='Nro Lote')
     debit2 = fields.Float(string="Debe")
-    
+
+    @api.depends('product_id', 'name', 'move_id.partner_id')
+    def get_invoice_name(self):
+        self.ensure_one()
+        if self.product_id and self.product_id.active_alternative:
+            partner = self.move_id.partner_id
+            if partner and partner not in self.product_id.excluyent_partner_ids:
+                name = f'[{self.product_id.default_code}] {self.product_id.name_alternative}'    
+                self.name = name
+                return name
+        return self.name
 
     @api.onchange('debit2')
     def onchange_debit2(self):
