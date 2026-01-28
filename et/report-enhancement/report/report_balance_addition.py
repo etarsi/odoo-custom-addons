a2e145498f0e78ef70947cc927d32f2265708363
ab-iceberg <rbmaldonado.1scz@gmail.com>
2026-01-28 10:39:13 -0300

    feat: se añadio el nuevo reporte

--- et/report-enhancement/report/report_balance_addition.py
+++ et/report-enhancement/report/report_balance_addition.py
@@ -2,7 +2,7 @@ from odoo import api, models
 
 
 class ReportBalanceAddition(models.AbstractModel):
-    _name = "report.report-enhancement.report_balance_addition"
+    _name = "report.report_enhancement.report_balance_addition"
     _description = "Balance de Sumas y Saldos (HTML/PDF)"
 
     @api.model
