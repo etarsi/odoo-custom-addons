a06ac1000a8971a91d940b8af07e02aa33c8bf60
ab-iceberg <rbmaldonado.1scz@gmail.com>
2026-01-28 13:23:03 -0300

    feat:se añadio nuevo reporte de saldos

--- et/report_enhancement/report/report_balance_addition.py
+++ et/report_enhancement/report/report_balance_addition.py
@@ -8,28 +8,7 @@ class ReportBalanceAddition(models.AbstractModel):
     _name = "report.report_enhancement.report_balance_addition"
     _description = "Balance de Sumas y Saldos (HTML/PDF)"
 
-    @api.model
-    def _get_report_values(self, docids, data=None):
-        data = data or {}
-        wiz = self.env["report.balance.addition.wizard"].browse((docids or [self.env.context.get("active_id")])[0])
 
-        lines, totals = wiz._get_lines()
-        currency = wiz.company_id.currency_id
-
-        return {
-            "docs": wiz,
-            "company": wiz.company_id,
-            "currency": currency,
-            "date_from": wiz.date_from,
-            "date_to": wiz.date_to,
-            "lines": lines,
-            "totals": totals,
-
-            # 🔥 asegura que formatLang sea función
-            "formatLang": lambda amount, currency_obj=None: _formatLang(
-                self.env, amount, currency_obj=(currency_obj or currency)
-            ),
-        }
         
     @api.model
     def _get_report_values(self, docids, data=None):
