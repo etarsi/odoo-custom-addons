7e861a5c6527d9742d5c7ed6b1cbd17f4961de1e
ab-iceberg <rbmaldonado.1scz@gmail.com>
2026-01-28 10:16:07 -0300

    feat: se añadio una mejora en el reporte de balance

--- et/report-enhancement/report/report_balance_addition.py
+++ et/report-enhancement/report/report_balance_addition.py
@@ -2,21 +2,19 @@ from odoo import api, models
 
 
 class ReportBalanceAddition(models.AbstractModel):
-    _name = "report.enhancement.report_balance_addition_document"
+    _name = "report.report-enhancement.report_balance_addition"
     _description = "Balance de Sumas y Saldos (HTML/PDF)"
 
     @api.model
     def _get_report_values(self, docids, data=None):
-        wizard = self.env["report.balance.addition.wizard"].browse(docids[:1])
-        lines, totals = wizard._get_lines()
+        wiz = self.env["report.balance.addition.wizard"].browse(docids[:1])
+        lines, totals = wiz._get_lines()
         return {
-            "doc_ids": wizard.ids,
-            "doc_model": wizard._name,
-            "docs": wizard,
-            "company": wizard.company_id,
-            "currency": wizard.company_id.currency_id,
-            "date_from": wizard.date_from,
-            "date_to": wizard.date_to,
+            "docs": wiz,
+            "company": wiz.company_id,
+            "currency": wiz.company_id.currency_id,
+            "date_from": wiz.date_from,
+            "date_to": wiz.date_to,
             "lines": lines,
             "totals": totals,
         }
