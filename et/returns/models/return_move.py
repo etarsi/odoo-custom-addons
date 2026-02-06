6cb91c5b3b6035f8e777f21ed93d1412961344d1
etarsi <tarsitano.ezquiel@hotmail.com>
2026-02-06 17:26:13 -0300

    fix

--- et/returns/models/return_move.py
+++ et/returns/models/return_move.py
@@ -101,8 +101,9 @@ class ReturnMove(models.Model):
 
                 cn = rm._create_cn_without_x2many(company, journal, document_type, invoice, return_lines)
                 cn.write({'return_move': rm.id})
+                
+                # rm.credit_note_ids = [(4, cn.id)]
                 created_moves |= cn
-
             return rm._action_open_credit_notes(created_moves)
 
 
@@ -213,6 +214,15 @@ class ReturnMove(models.Model):
         if len(credit_notes) == 1:
             action['views'] = [(self.env.ref('account.view_move_form').id, 'form')]
             action['res_id'] = credit_notes.id
+        return action
+    
+    def action_open_credit_notes(self):
+        self.ensure_one()
+        action = self.env.ref('account.action_move_out_refund_type').read()[0]
+        action['domain'] = [('id', 'in', self.credit_note_ids.ids)]
+        if len(self.credit_note_ids) == 1:
+            action['views'] = [(self.env.ref('account.view_move_form').id, 'form')]
+            action['res_id'] = self.credit_note_ids.id
         return action
     
 
