10f8ccca081a095c21d1906f21b3f52906ebec61
etarsi <tarsitano.ezquiel@hotmail.com>
2026-02-11 14:49:50 -0300

    action open transfer in sale order

--- et/wms_enhancement/models/sale_order.py
+++ et/wms_enhancement/models/sale_order.py
@@ -6,3 +6,17 @@ class SaleOrderInherit(models.Model):
 
 
     transfer_id = fields.Many2one(string="Transferencia", comodel_name="wms.transfer")
+
+    def action_open_wms_transfer(self):
+       self.ensure_one()
+       if not self.transfer_id:
+           return False
+
+       return {
+           'type': 'ir.actions.act_window',
+           'name': _('Transferencia WMS'),
+           'res_model': 'wms.transfer',
+           'view_mode': 'form',
+           'res_id': self.transfer_id.id,
+           'target': 'current',
+       }
