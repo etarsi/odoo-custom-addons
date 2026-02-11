b020d5b52f333ffd0ce81740b50e2eb4903d7754
etarsi <tarsitano.ezquiel@hotmail.com>
2026-02-11 15:04:33 -0300

    added dependency for 'product'

--- et/wms_enhancement/__manifest__.py
+++ et/wms_enhancement/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'sale', 'account'],
+    'depends': ['base', 'sale', 'account', 'product'],
 
     # always loaded
     'data': [
