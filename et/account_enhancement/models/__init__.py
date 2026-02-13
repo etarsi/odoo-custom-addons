700be8852b0f1a45ff3f36951e1545e20769c969
ab-iceberg <rbmaldonado.1scz@gmail.com>
2026-02-13 13:54:49 -0300

    feat: se añadio las correciones en account_enhancement

--- et/account_enhancement/models/__init__.py
+++ et/account_enhancement/models/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from . import models, sale_order, purchase_order, account_vat_ledger
+from . import fiscal_lock_mixin, models, sale_order, purchase_order, account_vat_ledger
 from . import report_factura_proveedor, res_partner, debt_composition, account_payment, account_payment_group
-from . import account_debit_note, account_fiscal_period_config, fiscal_lock_mixin
+from . import account_debit_note, account_fiscal_period_config
