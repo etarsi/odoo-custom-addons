82b400a934eab1ad3793873129e73128fd6b32a8
ab-iceberg <rbmaldonado.1scz@gmail.com>
2026-02-23 13:38:29 -0300

    feat: se añadio una nueva forma en el diseño de envio de correo de marketing

--- et/web_enhancement/models/__init__.py
+++ et/web_enhancement/models/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 
 from . import models, product_template, res_config_settings, ir_http_shop_gate, product_dowload_mark
-from . import marketing_promo_wizard
+from . import mail_marketing_design
