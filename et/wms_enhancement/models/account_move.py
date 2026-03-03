0b4c841a8ccb31e7db4caf32d7b4df844bd0b7f6
etarsi <tarsitano.ezquiel@hotmail.com>
2026-03-03 17:46:41 -0300

    .

--- et/wms_enhancement/models/account_move.py
+++ et/wms_enhancement/models/account_move.py
@@ -20,15 +20,3 @@ class AccountMoveInherit(models.Model):
            'res_id': self.task_id.id,
            'target': 'current',
         }
-    
-
-    def sacar_iva_21(self):
-        for move in self:
-            # 1) cambiar price_unit sin validar balance en cada write
-            for line in move.invoice_line_ids:
-                line.with_context(check_move_validity=False).write({
-                    'price_unit': line.price_unit / 1.21
-                })
-
-            # 2) recomputar todo lo dinámico (líneas contables, totales, vencimientos)
-            move.with_context(check_move_validity=False)._recompute_dynamic_lines()
