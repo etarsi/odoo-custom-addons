fad99a5ad164fe592c726278ea51c016de659168
ab-iceberg <rbmaldonado.1scz@gmail.com>
2026-03-05 14:42:45 -0300

    feat: se añadio unas mejoras en el vincular y crear hoja de ruta

--- et/wms_enhancement/wizard/tms_stock_picking_roadmap_wizard.py
+++ et/wms_enhancement/wizard/tms_stock_picking_roadmap_wizard.py
@@ -17,7 +17,7 @@ class TmsStockPickingRoadmapWizard(models.TransientModel):
     roadmap_id = fields.Many2one("tms.roadmap", string="Hoja de Ruta (existente)")
     transport_id = fields.Many2one("tms.transport", string="Vehículo")
     empresa_id = fields.Many2one("delivery.carrier", string="Empresa")
-    industry_ids = fields.Many2many("res.partner.industry", string="Zonas")
+    industry_ids = fields.Many2many("res.partner.industry", string="Zonas", compute="_compute_industry_ids", store=True, tracking=True)
     assistants = fields.Integer(string="Ayudantes", default=0)
 
     line_ids = fields.One2many(
@@ -31,6 +31,11 @@ class TmsStockPickingRoadmapWizard(models.TransientModel):
         compute="_compute_total_bulk_defendant",
         store=False,
     )
+    
+    def _compute_industry_ids(self):
+        for rec in self:
+            industry_ids = rec.stock_picking_ids.mapped('industry_id').ids
+            rec.industry_ids = [(6, 0, industry_ids)]
 
     @api.depends("stock_picking_ids")
     def _compute_total_bulk_defendant(self):
@@ -112,9 +117,11 @@ class TmsStockPickingRoadmapWizardLine(models.TransientModel):
     tms_stock_picking_id = fields.Many2one("tms.stock.picking", string="Ruteo", required=True)
     wms_task_id = fields.Many2one("wms.task", string="Tarea de picking")
     partner_id = fields.Many2one("res.partner", string="Cliente")
+    industry_id = fields.Many2one("res.partner.industry", string="Zona")
     direction = fields.Char(string="Dirección")
     bulto_defendant = fields.Float(string="Bultos Demandados")
     bulto_picking = fields.Float(string="Bultos Pickeados")
+    
     
     
     @api.depends("tms_stock_picking_id")
@@ -123,6 +130,7 @@ class TmsStockPickingRoadmapWizardLine(models.TransientModel):
             if rec.tms_stock_picking_id:
                 rec.wms_task_id = rec.tms_stock_picking_id.wms_task_id if rec.tms_stock_picking_id.wms_task_id else False
                 rec.partner_id = rec.tms_stock_picking_id.partner_id if rec.tms_stock_picking_id.partner_id else False
+                rec.industry_id = rec.tms_stock_picking_id.industry_id if rec.tms_stock_picking_id.industry_id else False
                 rec.direction = getattr(rec.tms_stock_picking_id, "direccion_entrega", False) or getattr(rec.tms_stock_picking_id, "direction", False)
                 rec.bulto_defendant = getattr(rec.tms_stock_picking_id, "cantidad_bultos", 0.0) or 0.0
                 rec.bulto_picking = getattr(rec.tms_stock_picking_id, "bulk_picking", 0.0) or 0.0
