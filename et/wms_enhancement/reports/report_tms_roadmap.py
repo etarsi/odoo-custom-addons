bf988d6646e181effb1e113d4e74936da2fbe65e
ab-iceberg <rbmaldonado.1scz@gmail.com>
2026-03-31 14:30:56 -0300

    feat: se añadio las condiciones sobre el padron y sale _enhancement

--- et/wms_enhancement/reports/report_tms_roadmap.py
+++ et/wms_enhancement/reports/report_tms_roadmap.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from odoo import api, models
+import logging
+_logger = logging.getLogger(__name__)
 
 
 class ReportTmsRoadmap(models.AbstractModel):
@@ -10,7 +12,9 @@ class ReportTmsRoadmap(models.AbstractModel):
     def _get_report_values(self, docids, data=None):
         docs = self.env["tms.roadmap"].browse(docids)
         data = data or {}
-
+        _logger.warning(f"Data recibida en el reporte: {data}")
+        _logger.warning(f"Documentos recibidos en el reporte: {docs.ids}")
+        _logger.warning(f"Contexto del reporte: {self.env.context}")
         return {
             "doc_ids": docs.ids,
             "doc_model": "tms.roadmap",
