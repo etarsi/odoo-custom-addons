78bb5a5e22eb22d7140443e586abdf585699bf40
ab-iceberg <rbmaldonado.1scz@gmail.com>
2026-04-10 15:24:44 -0300

    feat: se realizo las mejoras para el de ventas de impuestos

--- et/account_enhancement/models/__init__.py
+++ et/account_enhancement/models/__init__.py
@@ -2,4 +2,4 @@
 
 from . import models, sale_order, purchase_order, account_vat_ledger
 from . import report_factura_proveedor, res_partner, debt_composition, account_payment, account_payment_group
-from . import account_debit_note, account_fiscal_period_config, account_export_arba
+from . import account_debit_note, account_fiscal_period_config, account_export_arba, res_partner_arba_alicuot
