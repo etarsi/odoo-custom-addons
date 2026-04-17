2621aecb70928ab2f73ae0ebcd72840cc397e8f9
ab-iceberg <rbmaldonado.1scz@gmail.com>
2026-04-17 12:27:22 -0300

    feat: se añadio las correcciones a las lineas de transferencias

--- et/wms_enhancement/models/sale_order.py
+++ et/wms_enhancement/models/sale_order.py
@@ -148,5 +148,6 @@ class SaleOrderLineInherit(models.Model):
                 for transfer_line in transfer_lines:
                     #solo actualizar las lineas de tarea que no han sido procesadas
                     line_task_lines = self.env['wms.task.line'].search([('transfer_line_id', '=', transfer_line.id), ('task_id.digip_state', '=', 'no')])
-                    line_task_lines.write({'quantity': line.product_uom_qty})
+                    if line_task_lines:
+                        line_task_lines.write({'quantity': line.product_uom_qty})
                 transfer_line.update_qty_pending_done_invoiced()
